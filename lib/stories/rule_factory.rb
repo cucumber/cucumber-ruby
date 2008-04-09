77c23c0247c0bedf023079d8366dceb2c1dffd13
unknown <Aslak Hellesy@.BEKK.no>
2008-04-09 23:17:42 +0200

    simple conversion of rules is working

--- lib/stories/rule_factory.rb
+++ lib/stories/rule_factory.rb
@@ -1,9 +1,20 @@
 module Stories
   # Translates from "user" rules to Treetop rules
   class RuleFactory
+
     # Translates +step_expression+ to a Treetop rule body
     def rule_for(step_expression)
-      "'I was ' word ' and ' word"
+      s = StringScanner.new(step_expression)
+      rule = ""
+      while !s.eos?
+        if t = s.scan(/[^\$]*/)
+          rule << "'#{t}'" 
+        end
+        if t = s.scan(/[\$]\w*/)
+          rule << " word " 
+        end
+      end
+      rule.strip
     end
   end
 end
--- spec/stories/rule_factory_spec.rb
+++ spec/stories/rule_factory_spec.rb
@@ -6,6 +6,10 @@ module Stories
       @rf = RuleFactory.new
     end
     
+    it "should translate a rule with zero arguments" do
+      @rf.rule_for("hello world").should == "'hello world'"
+    end
+
     it "should translate a rule with two arguments" do
       @rf.rule_for("I was $one and $two").should == "'I was ' word ' and ' word"
     end
