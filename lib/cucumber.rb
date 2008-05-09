a68f6b399730d65567e227a171e9a6251b8908b3
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-05-09 20:45:01 +0200

    updated manifest

--- lib/cucumber.rb
+++ lib/cucumber.rb
@@ -1,5 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+require 'rubygems'
 require 'treetop'
 require 'cucumber/version'
 require 'cucumber/cli'
