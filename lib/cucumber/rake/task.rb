9873d6c48511eca80cbf462def87eb6bdc6851f7
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-08-17 14:56:38 +0200

    Added support for RCov in Rake task

--- lib/cucumber/rake/task.rb
+++ lib/cucumber/rake/task.rb
@@ -1,7 +1,6 @@
 module Cucumber
   module Rake
     # Defines a task for running features.
-    # TODO: Base on http://github.com/dchelimsky/rspec/tree/master/lib/spec/rake/spectask.rb
     class Task
       LIB    = File.expand_path(File.dirname(__FILE__) + '/../..')
       BINARY = File.expand_path(File.dirname(__FILE__) + '/../../../bin/cucumber')
@@ -12,11 +11,14 @@ module Cucumber
       attr_accessor :feature_list
       attr_accessor :feature_pattern
       attr_accessor :cucumber_opts
+      attr_accessor :rcov
+      attr_accessor :rcov_opts
       
       # Define a task
       def initialize(task_name = "features", desc = "Run Features")
         @task_name, @desc = task_name, desc
-        @libs = [LIB]
+        @libs = []
+        @rcov_opts = %w{--rails --exclude osx\/objc,gems\/}
 
         yield self if block_given?
 
@@ -28,27 +30,28 @@ module Cucumber
       def define_tasks
         desc @desc
         task @task_name do
-          args = []
-          args << '-I'
-          args << '"%s"' % libs.join(File::PATH_SEPARATOR)
-          args << '"%s"' % BINARY
-          args << (ENV['CUCUMBER_OPTS'] || cucumber_opts)
+          lib_args     = ['"%s"' % ([LIB] + libs).join(File::PATH_SEPARATOR)]
+          cucumber_bin = ['"%s"' % BINARY]
+          cuc_opts     = [(ENV['CUCUMBER_OPTS'] || cucumber_opts)]
 
           step_files.each do |step_file|
-            args << '--require'
-            args << step_file
+            cuc_opts << '--require'
+            cuc_opts << step_file
           end
-          args << feature_files
-          args.flatten!
-          args.compact!
+
+          if rcov
+            args = (['-I'] + lib_args + ['-S', 'rcov'] + rcov_opts + cucumber_bin + ['--'] + cuc_opts + feature_files).flatten
+          else
+            args = (['-I'] + lib_args + cucumber_bin + cuc_opts + feature_files).flatten
+          end
           ruby(args.join(" ")) # ruby(*args) is broken on Windows
         end
       end
 
 
       def feature_files # :nodoc:
-        if ENV['STORY']
-          FileList[ ENV['STORY'] ]
+        if ENV['FEATURE']
+          FileList[ ENV['FEATURE'] ]
         else
           result = []
           result += feature_list.to_a if feature_list
