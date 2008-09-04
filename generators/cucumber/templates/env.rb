84e7e22eb24c1223dcdd37ab58d028c90aeada3e
Daniel Lucraft <dan@fluentradical.com>
2008-09-04 14:58:17 +0100

    Made transactional fixtures optional. To use transactional fixtures put Cucumber::Rails.use_transactional_fixtures into features/steps/env.rb after cucumber/rails/world is required.

--- generators/cucumber/templates/env.rb
+++ generators/cucumber/templates/env.rb
@@ -2,5 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
+Cucumber::Rails.use_transactional_fixtures
+
 # Comment out the next line if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
