69b3cc6561feb6186b29958fdffa2bd2033de816
David Chelimsky <dchelimsky@gmail.com>
2008-09-17 19:09:00 -0500

    tighten up regexps

--- generators/cucumber/templates/common_webrat.rb
+++ generators/cucumber/templates/common_webrat.rb
@@ -1,23 +1,23 @@
 # Commonly used webrat steps
 # http://github.com/brynary/webrat
 
-When /I press "(.*)"/ do |button|
+When /^I press "(.*)"$/ do |button|
   clicks_button(button)
 end
 
-When /I follow "(.*)"/ do |link|
+When /^I follow "(.*)"$/ do |link|
   clicks_link(link)
 end
 
-When /I fill in "(.*)" with "(.*)"/ do |field, value|
+When /^I fill in "(.*)" with "(.*)"$/ do |field, value|
   fills_in(field, :with => value) 
 end
 
-When /I check "(.*)"/ do |field|
+When /^I check "(.*)"$/ do |field|
   checks(field) 
 end
 
-When /I go to (.*)/ do |page|
+When /^I go to (.*)$/ do |page|
   visits case page
   when "the home page"
     "/"
@@ -26,10 +26,10 @@ When /I go to (.*)/ do |page|
   end
 end
 
-Then /I should see "(.*)"/ do |text|
+Then /^I should see "(.*)"$/ do |text|
   response.body.should =~ /#{text}/m
 end
 
-Then /I should not see "(.*)"/ do |text|
+Then /^I should not see "(.*)"$/ do |text|
   response.body.should_not =~ /#{text}/m
 end
