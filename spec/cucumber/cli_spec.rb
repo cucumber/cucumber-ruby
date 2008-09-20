50493b5e1a318c2b501ba1f63971811cacb08cb9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-09-20 22:16:57 +0200

    Use --profile in calculator example

--- spec/cucumber/cli_spec.rb
+++ spec/cucumber/cli_spec.rb
@@ -17,5 +17,20 @@ module Cucumber
         :lang => 'en'
       }
     end
+
+    it "should expand args from YAML file's default if there are no args" do
+      cli = CLI.new
+
+      cucumber_yml = {'default' => '--require from/yml'}.to_yaml
+      IO.should_receive(:read).with('cucumber.yml').and_return(cucumber_yml)
+
+      cli.parse_options!([])
+      cli.options.should == {
+        :format => 'pretty',
+        :require => ['from/yml'], 
+        :dry_run => false, 
+        :lang => 'en'
+      }
+    end
   end
 end
