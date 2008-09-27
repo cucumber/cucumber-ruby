cucumber/cucumber-ruby

commit 9d001233a7e0b7167064c45c7f3b63db2bc7e6b5

Namespace Watir

examples/watir/features/steps/stories_steps.rb
@@ -3,13 +3,13 @@ require 'spec'
 case PLATFORM
 when /darwin/
   require 'safariwatir'
-  Browser = Watir::Safari
+  Watir::Browser = Watir::Safari
 when /win32|mingw/
   require 'watir'
-  Browser = Watir::IE
+  Watir::Browser = Watir::IE
 when /java/
   require 'celerity'
-  Browser = Celerity::Browser
+  Watir::Browser = Celerity::Browser
 else
   raise "This platform is not supported (#{PLATFORM})"
 end
@@ -30,7 +30,7 @@ class GoogleSearch
 end
 
 Before do
-  @b = Browser.new
+  @b = Watir::Browser.new
 end
 
 After do

commit afa13ab8e132145c244a19665d61c0af5ef423a9

Make Watir example run on Celerity again (broken by 9d00123).

examples/watir/features/steps/stories_steps.rb
@@ -9,7 +9,7 @@ when /win32|mingw/
   Watir::Browser = Watir::IE
 when /java/
   require 'celerity'
-  Watir::Browser = Celerity::Browser
+  module Watir; Browser = Celerity::Browser; end
 else
   raise "This platform is not supported (#{PLATFORM})"
 end