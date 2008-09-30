81fe41bc5f6f89bb31afede94664db938d997165
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-09-30 07:50:05 +0200

    Allow several tables in the same feature [#16 state:resolved]

--- spec/cucumber/treetop_parser/feature_parser_spec.rb
+++ spec/cucumber/treetop_parser/feature_parser_spec.rb
@@ -73,6 +73,13 @@ module Cucumber
         f.scenarios[1].should have(0).steps
         f.scenarios[2].should have(1).steps
       end
+
+      it "should allow multiple tables" do
+        p = FeatureParser.new
+        f = p.parse_feature(File.dirname(__FILE__) + '/multiple_tables.feature')
+        f.should have(6).scenarios
+        f.scenarios[0].should have(5).steps
+      end
       
     end
   end
