e195ab84c962ad8fb17d6e8a570adbecc01fea8c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-10-05 16:03:16 +0200

    Enable English by default when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,7 @@ require 'spec'
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'cucumber'
 require 'cucumber/treetop_parser/feature_en'
-
-# Prevent CLI's exit hook from running
-class Cucumber::CLI
-  def self.execute_called?
-    true
-  end
-end
+Cucumber.load_language('en')
 
 # Open up the tree classes a little for easier inspection.
 module Cucumber
