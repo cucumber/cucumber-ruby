feb8f2fecf785bf98972e36eda1c21db74d8ba9e
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-10-07 18:46:10 +0200

    shuffled rails specs around a little

--- spec/cucumber/rails/world_spec.rb
+++ spec/cucumber/rails/world_spec.rb
@@ -1,10 +1,10 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-$:.unshift(File.dirname(__FILE__) + '/resources')
+require File.dirname(__FILE__) + '/../../spec_helper'
+$:.unshift(File.dirname(__FILE__) + '/stubs')
 
 describe "Rails world" do
 
   it "should run without Test::Unit.run defined" do
-    require "mocks"
+    require "mini_rails"
     require "cucumber/rails/world"
   end
 
--- spec/cucumber/resources/test_help.rb
+++
@@ -1,1 +0,0 @@
-# this file is included by rails/world.rb
