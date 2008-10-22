b1a83c8d567bfa465f0fc2d505a9a253b2abee25
Joseph Wilk <josephwilk@joesniff.co.uk>
2008-10-22 12:07:33 +0100

    Make Broadcaster live in Cucumber module

--- lib/cucumber/broadcaster.rb
+++ lib/cucumber/broadcaster.rb
@@ -1,17 +1,19 @@
-class Broadcaster
+module Cucumber
+  class Broadcaster
 
-  def initialize(receivers = [])
-    @receivers = receivers
-  end
-  
-  def register(receiver)
-    @receivers << receiver
-  end
-  
-  def method_missing(method_name, *args)
-    @receivers.each  do |receiver|
-      receiver.__send__(method_name, *args) if receiver.respond_to?(method_name)
+    def initialize(receivers = [])
+      @receivers = receivers
     end
+
+    def register(receiver)
+      @receivers << receiver
+    end
+
+    def method_missing(method_name, *args)
+      @receivers.each  do |receiver|
+        receiver.__send__(method_name, *args) if receiver.respond_to?(method_name)
+      end
+    end
+
   end
-  
 end
