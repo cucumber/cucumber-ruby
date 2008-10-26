7f2557ddd913d3d6e79cb76eedb2127960db3308
Reza Primardiansyah <reza.primardiansyah@gmail.com>
2008-10-26 17:27:54 +0100

    Added Indonesian

---
+++ examples/indonesian_kalkulator/features/steps/calculator_steps.rb
@@ -0,0 +1,31 @@
+require 'spec'
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'calculator'
+
+Before do
+  @calc = Calculator.new
+end
+
+After do
+end
+
+Given "aku sudah masukkan $n ke kalkulator" do |n|
+  @calc.push n.to_i
+end
+
+When /aku tekan (\w+)/ do |op|
+  @result = @calc.send op
+end
+
+Then /hasilnya harus (.*) di layar/ do |result|
+  @result.should == result.to_f
+end
+
+Then /class hasilnya harus (\w*)/ do |class_name|
+  @result.class.name.should == class_name
+end
+
+Given /it should rain on (\w+)/ do |day|
+  @calc.rain?(day).should == true
+end
+
