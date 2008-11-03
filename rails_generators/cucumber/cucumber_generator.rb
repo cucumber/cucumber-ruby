1ca6276efbc67f22af36ea4e9cd4d66f862975e3
David Chelimsky <dchelimsky@gmail.com>
2008-11-03 07:18:32 -0600

    cleaner output from cucumber_generator

--- rails_generators/cucumber/cucumber_generator.rb
+++ rails_generators/cucumber/cucumber_generator.rb
@@ -6,16 +6,18 @@ class CucumberGenerator < Rails::Generator::Base
                               Config::CONFIG['ruby_install_name'])
   def manifest
     record do |m|
-      script_options     = { :chmod => 0755, :shebang => options[:shebang] == DEFAULT_SHEBANG ? nil : options[:shebang] }
-
       m.directory 'features/step_definitions'
-      m.directory 'features/support'
-      m.file      'env.rb', 'features/support/env.rb'
       m.file      'webrat_common.rb', 'features/step_definitions/webrat_common.rb'
 
+      m.directory 'features/support'
+      m.file      'env.rb',           'features/support/env.rb'
+
       m.directory 'lib/tasks'
-      m.file      'cucumber.rake', 'lib/tasks/cucumber.rake'
-      m.file      'cucumber',      'script/cucumber',        script_options
+      m.file      'cucumber.rake',    'lib/tasks/cucumber.rake'
+
+      m.file      'cucumber',         'script/cucumber', {
+        :chmod => 0755, :shebang => options[:shebang] == DEFAULT_SHEBANG ? nil : options[:shebang]
+      }
     end
   end
 
