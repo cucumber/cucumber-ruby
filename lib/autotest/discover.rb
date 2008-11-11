aab6bae55cf2cb8ef11fc038b14953e81c3d256e
Peter Jaros <peeja@dropio.com>
2008-11-11 15:34:04 -0500

    Add warning when AUTOFEATURE is disabled.

--- lib/autotest/discover.rb
+++ lib/autotest/discover.rb
@@ -1,3 +1,9 @@
 Autotest.add_discovery do
-  "cucumber" if ENV['AUTOFEATURE'] == 'true' && File.directory?('features')
+  if File.directory?('features')
+    if ENV['AUTOFEATURE'] == 'true'
+      "cucumber"
+    else
+      puts "(Not running features.  To run features in autotest, set AUTOFEATURE=true.)"
+    end
+  end
 end
