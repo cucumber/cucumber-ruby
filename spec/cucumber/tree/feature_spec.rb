167068245a53629cd3ede6cc9d7353aabc3f6907
Joseph Wilk <josephwilk@joesniff.co.uk>
2008-11-15 14:02:16 +0000

    Features written using Ruby where breaking due to missing a line number

--- spec/cucumber/tree/feature_spec.rb
+++ spec/cucumber/tree/feature_spec.rb
@@ -3,10 +3,20 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 module Cucumber
   module Tree
     describe Feature do
+      
       it "should have padding_length 2 when alone" do
         feature = Feature.new('header')
         feature.padding_length.should == 2
       end
+      
+      it "should create a new scenario for a feature" do
+        feature = Feature.new('header')
+        
+        Scenario.should_receive(:new).with(feature, 'test scenario', "17")
+        
+        feature.Scenario('test scenario') {}
+      end
+      
     end
   end
 end
