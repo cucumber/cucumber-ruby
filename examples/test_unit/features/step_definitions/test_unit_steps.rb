22954317893a90ce5fde3e2cf337d7296604152f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-11-23 18:50:37 +0100

    Yield existing world object to World block [#87 state:resolved milestone_id:0.1.10]

---
+++ examples/test_unit/features/step_definitions/test_unit_steps.rb
@@ -0,0 +1,15 @@
+require 'test/unit/assertions'
+World do |o|
+  o.extend(Test::Unit::Assertions)
+  o
+end
+
+Given /^(\w+) = (\w+)$/ do |var, value|
+  instance_variable_set("@#{var}", value)
+end
+
+Then /^I can assert that (\w+) == (\w+)$/ do |var_a, var_b|
+  a = instance_variable_get("@#{var_a}")
+  b = instance_variable_get("@#{var_b}")
+  assert_equal(a, b)
+end
