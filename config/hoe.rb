1594dff54ad23815a8053c3966146a9a8c705c55
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-11-23 16:54:59 +0100

    Make RSpec a development dependency only

--- config/hoe.rb
+++ config/hoe.rb
@@ -57,7 +57,8 @@ $hoe = Hoe.new(GEM_NAME, VERS) do |p|
   # == Optional
   p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
   #p.extra_deps = []     # An array of rubygem dependencies [name, version], e.g. [ ['active_support', '>= 1.3.1'] ]
-  p.extra_deps = [ ['term-ansicolor', '>= 1.0.3'], ['treetop', '>= 1.2.4'], ['rspec', '>= 1.1.5'], ['diff-lcs', '>= 1.1.2'] ]
+  p.extra_deps = [ ['term-ansicolor', '>= 1.0.3'], ['treetop', '>= 1.2.4'], ['diff-lcs', '>= 1.1.2'] ]
+  p.extra_dev_deps = [ ['rspec', '>= 1.1.11'] ]
 
   #p.spec_extras = {}    # A hash of extra values to set in the gemspec.
   
