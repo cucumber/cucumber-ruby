834f8c0524f164dd33f8aac0bd8e7c9e6f043590
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-11-24 18:10:48 +0100

    Added Matchy to the Test::Unit example

--- examples/test_unit/features/step_definitions/test_unit_steps.rb
+++ examples/test_unit/features/step_definitions/test_unit_steps.rb
@@ -8,8 +8,19 @@ Given /^(\w+) = (\w+)$/ do |var, value|
   instance_variable_set("@#{var}", value)
 end
 
-Then /^I can assert that (\w+) == (\w+)$/ do |var_a, var_b|
-  a = instance_variable_get("@#{var_a}")
-  b = instance_variable_get("@#{var_b}")
-  assert_equal(a, b)
+begin
+  require 'rubygems'
+  require 'matchy'
+  Then /^I can assert that (\w+) == (\w+)$/ do |var_a, var_b|
+    a = instance_variable_get("@#{var_a}")
+    b = instance_variable_get("@#{var_b}")
+    a.should == b
+  end
+rescue LoadError
+  STDERR.puts "***** You should install matchy *****"
+  Then /^I can assert that (\w+) == (\w+)$/ do |var_a, var_b|
+    a = instance_variable_get("@#{var_a}")
+    b = instance_variable_get("@#{var_b}")
+    assert_equal(a, b)
+  end
 end
