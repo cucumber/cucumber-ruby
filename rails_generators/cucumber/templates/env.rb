81a720a15c9cc7a07feba0b65d323e6d76e6fa96
Bryan Helmkamp <bryan@brynary.com>
2008-12-01 09:55:14 -0500

    Add require for webrat/rspec-rails to generated env.rb
    This includes Webrat's contain, have_selector and have_xpath matchers

--- rails_generators/cucumber/templates/env.rb
+++ rails_generators/cucumber/templates/env.rb
@@ -4,7 +4,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
 Cucumber::Rails.use_transactional_fixtures
 
-# Comment out the next line if you're not using RSpec's matchers (should / should_not) in your steps.
-require 'cucumber/rails/rspec'
+require 'webrat/rails'
 
-require "webrat/rails"
+# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
+require 'cucumber/rails/rspec'
+require 'webrat/rspec-rails'
