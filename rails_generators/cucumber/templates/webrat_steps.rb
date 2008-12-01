699600aa8b7c00eb7c109a4c1a6f0b03c6767755
Bryan Helmkamp <bryan@brynary.com>
2008-11-30 23:59:44 -0500

    Require webrat/rails from generated env.rb. This is now required by Webrat

--- rails_generators/cucumber/templates/webrat_steps.rb
+++ rails_generators/cucumber/templates/webrat_steps.rb
@@ -1,8 +1,6 @@
 # Commonly used webrat steps
 # http://github.com/brynary/webrat
 
-require 'webrat' if !defined?(Webrat) # Because some people have it installed as a Gem
-
 When /^I press "(.*)"$/ do |button|
   clicks_button(button)
 end
