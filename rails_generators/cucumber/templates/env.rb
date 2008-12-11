e134a21ac40a14dcf561050ba2f5ef99f206c3de
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2008-12-11 15:48:17 +0100

    Added Unicode support for windows and fixed [#81] for real I hope

--- rails_generators/cucumber/templates/env.rb
+++ rails_generators/cucumber/templates/env.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
+require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
 Cucumber::Rails.use_transactional_fixtures
 
 require 'webrat/rails'
