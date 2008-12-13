a705d881219432c3be86f2ca1b34d9d83fea5f73
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-12-13 14:01:02 +0100

    Started on new grammar

---
+++ spec/cucumber/parser/table_spec.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require 'treetop'
+require 'cucumber/parser/table'
+
+module Cucumber
+  module Parser
+    describe Table do
+      before do
+        @parser = TableParser.new
+      end
+      
+      def parse(text)
+        @parser.parse(text) || raise(@parser.failure_reason)
+      end
+
+      it "should parse a row with one cell" do
+        parse("hi")
+      end
+
+      it "should parse a row with two cells" do
+        parse("hello|there")
+      end
+    end
+  end
+end
