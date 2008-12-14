dd7f8513a315a0ecb0d7d78c46bbab728323db53
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-12-14 03:38:37 +0100

    Started the feature parser

---
+++ spec/cucumber/parser/feature_spec.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+require 'treetop'
+require 'cucumber/parser/basic'
+require 'cucumber/parser/table'
+require 'cucumber/parser/feature'
+require 'cucumber/ast'
+
+module Cucumber
+  module Parser
+    describe Feature do
+      before do
+        @parser = FeatureParser.new
+      end
+      
+      def parse(text)
+        tree = @parser.parse(text)
+        raise(@parser.failure_reason) if tree.nil?
+        tree.build
+      end
+      
+      it "should parse a file with only comments" do
+        parse("# My comment").comment.should == "# My comment"
+      end
+    end
+  end
+end
