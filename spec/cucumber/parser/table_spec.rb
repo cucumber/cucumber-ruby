68024593d6dc7234fbcce7a24a0fd9a069518c97
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-12-14 01:51:22 +0100

    Don't use recursive rules - now parses tables quite well

--- spec/cucumber/parser/table_spec.rb
+++ spec/cucumber/parser/table_spec.rb
@@ -13,8 +13,20 @@ module Cucumber
         @parser.parse(text) || raise(@parser.failure_reason)
       end
 
-      it "should parse a 1x2 table" do
+      it "should parse a 1x2 table with newline" do
+        parse(" | 1 | 2 | \n").build.should == [%w{1 2}]
+      end
+
+      it "should parse a 1x2 table without newline" do
         parse("| 1 | 2 |").build.should == [%w{1 2}]
+      end
+
+      it "should parse a 2x2 table" do
+        parse("| 1 | 2 |\n| 3 | 4 |").build.should == [%w{1 2}, %w{3 4}]
+      end
+
+      it "should parse a 2x2 table with empty cells" do
+        parse("| 1 |  |\n|| 4 |").build.should == [['1', nil], [nil, '4']]
       end
 
     end
