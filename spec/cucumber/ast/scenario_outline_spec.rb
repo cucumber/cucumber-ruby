8daf32e55d00d0358b772c32b9d3b5fbbfdafcc0
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-12-21 01:27:20 +0100

    Updated spec to reflect current 0.1.13 behaviour

--- spec/cucumber/ast/scenario_outline_spec.rb
+++ spec/cucumber/ast/scenario_outline_spec.rb
@@ -6,7 +6,17 @@ require 'cucumber/core_ext/string'
 module Cucumber
   module Ast
     describe ScenarioOutline do
-      it "should execute outline steps when row matches 1 step" do
+      # Scenario Outline: reusing place holder
+      #   Given there are <start> cucumbers
+      #   When I eat <eat> cucumbers
+      #   Then I should have <left> cucumbers
+      #   And I should have <eat> cucumbers in my belly
+      # 
+      # Examples:
+      #   | start  | eat  | left |
+      #   |  12    |  5   |  7   |
+      #   |  20    |  6   |  14  |
+      it "should reuse variables" do
         step_mother = mock("StepMother")
 
         scenario_outline = ScenarioOutline.new(
@@ -14,51 +24,38 @@ module Cucumber
           Tags.new([]),
           "My outline",
           [
-            Step.new(step_mother, 'Given', 'a <color> <veggie>')
+            Step.new(step_mother, 'Given', 'there are <start> cucumbers'),
+            Step.new(step_mother, 'When',  'I eat <eat> cucumbers'),
+            Step.new(step_mother, 'Then',  'I should have <left> cucumbers'),
+            Step.new(step_mother, 'And',   'I should have <eat> cucumbers in my belly')
           ],
           [
-            %w{green cucumber},
-            %w{red tomato}
-          ]
-        )
-        
-        step_mother.should_receive(:execute_step_by_name).with("a green cucumber", anything)
-        step_mother.should_receive(:execute_step_by_name).with("a red tomato", anything)
-        visitor = Visitor.new
-        scenario_outline.accept(visitor)
-
-        # Just keeping this code for debugging (output)
-        #
-        # require 'cucumber/formatter/pretty'
-        # mom = Object.new
-        # mom.extend(StepMom)
-        # visitor = Formatter::Pretty.new(mom, STDOUT)
-        # visitor.visit_feature_element(scenario_outline)
-      end
-
-      it "should execute outline steps when row matches 2 steps" do
-        step_mother = mock("StepMother")
-
-        scenario_outline = ScenarioOutline.new(
-          Comment.new(""),
-          Tags.new([]),
-          "My outline",
-          [
-            Step.new(step_mother, 'Given', 'a <color> glove'),
-            Step.new(step_mother, 'Given', 'a tasty <veggie>')
-          ],
-          [
-            %w{green cucumber},
-            %w{red tomato}
+            %w{start eat left},
+            %w{12 5 7},
+            %w{20 6 14}
           ]
         )
 
-        step_mother.should_receive(:execute_step_by_name).with("a green glove", anything)
-        step_mother.should_receive(:execute_step_by_name).with("a tasty cucumber", anything)
-        step_mother.should_receive(:execute_step_by_name).with("a red glove", anything)
-        step_mother.should_receive(:execute_step_by_name).with("a tasty tomato", anything)
-        visitor = Visitor.new
-        scenario_outline.accept(visitor)
+        step_mother.should_receive(:execute_step_by_name).with("there are 12 cucumbers", anything)
+        step_mother.should_receive(:execute_step_by_name).with("I eat 5 cucumbers", anything)
+        step_mother.should_receive(:execute_step_by_name).with("I should have 7 cucumbers", anything)
+        step_mother.should_receive(:execute_step_by_name).with("I should have 5 cucumbers in my belly", anything)
+
+        step_mother.should_receive(:execute_step_by_name).with("there are 20 cucumbers", anything)
+        step_mother.should_receive(:execute_step_by_name).with("I eat 6 cucumbers", anything)
+        step_mother.should_receive(:execute_step_by_name).with("I should have 14 cucumbers", anything)
+        step_mother.should_receive(:execute_step_by_name).with("I should have 6 cucumbers in my belly", anything)
+
+        if ENV['CUCUMBER_DEBUG']
+          require 'cucumber/formatter/pretty'
+          mom = Object.new
+          mom.extend(StepMom)
+          visitor = Formatter::Pretty.new(mom, STDOUT)
+          visitor.visit_feature_element(scenario_outline)
+        else
+          visitor = Visitor.new
+          visitor.visit_feature_element(scenario_outline)
+        end
       end
     end
   end
