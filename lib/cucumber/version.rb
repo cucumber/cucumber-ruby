eec358de5ac31176726cfe1322b0f59f7c26f184
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2008-12-22 00:46:10 +0100

    Post patch release

--- lib/cucumber/version.rb
+++ lib/cucumber/version.rb
@@ -3,7 +3,7 @@ module Cucumber #:nodoc:
     MAJOR = 0
     MINOR = 1
     TINY  = 13
-    PATCH = 1
+    PATCH = 2 # Set to nil for official release
 
     STRING = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
   end
