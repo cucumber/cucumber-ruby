eb0bae67258eea970b630514f3d9340205e10875
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-01-07 01:21:17 +0100

    Added scenario for Norwegian

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -1,6 +1,11 @@
+Given /^I am in (.*)$/ do |dir|
+  @dir = dir
+end
+
 When /^I run cucumber (.*)$/ do |cmd|
-  dir = File.expand_path(File.dirname(__FILE__) + '/../../examples/self_test')
-  Dir.chdir(dir) do
+  @dir ||= 'self_test'
+  full_dir ||= File.expand_path(File.dirname(__FILE__) + "/../../examples/#{@dir}")
+  Dir.chdir(full_dir) do
     @full_cmd = "#{Cucumber::RUBY_BINARY} #{Cucumber::BINARY} #{cmd}"
     @out = `#{@full_cmd}`
   end
