b54c5f55ec2b660769e6f7d4d3ef0813c3586893
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-01-11 16:30:13 +0100

    Remove old code

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -8,9 +8,14 @@ When /^I run cucumber (.*)$/ do |cmd|
   Dir.chdir(full_dir) do
     @full_cmd = "#{Cucumber::RUBY_BINARY} #{Cucumber::BINARY} #{cmd}"
     @out = `#{@full_cmd}`
+    @status = $?.exitstatus
   end
 end
 
 Then /^the output should be$/ do |output|
   @out.should == output
+end
+
+Then /^exit code should be (\d+)$/ do |code|
+  @status.should == code.to_i
 end
