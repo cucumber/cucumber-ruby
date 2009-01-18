c8c30b8b2ff7ecddb973113d8b4ab4d7008086db
Bryan Helmkamp <bryan@brynary.com>
2009-01-18 17:04:36 -0500

    Add webrat step for "When I go to ..." and a page name to path mapping file

--- rails_generators/cucumber/templates/webrat_steps.rb
+++ rails_generators/cucumber/templates/webrat_steps.rb
@@ -1,5 +1,11 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
 # Commonly used webrat steps
 # http://github.com/brynary/webrat
+
+When /^I go to (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
 
 When /^I press "(.*)"$/ do |button|
   click_button(button)
