46c9856aac71ce106acbe09c2f7f5c642d44588d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-01-26 01:04:09 +0100

    Release 0.1.99.6 - better parse errors

--- lib/cucumber/parser/treetop_ext.rb
+++ lib/cucumber/parser/treetop_ext.rb
@@ -28,13 +28,9 @@ module Cucumber
     class SyntaxError < StandardError
       def initialize(parser, file, line_offset)
         tf = parser.terminal_failures
-        expected = tf.size == 1 ? tf[0].expected_string.inspect : "one of #{tf.map{|f| f.expected_string.inspect}.uniq*', '}"
-        after = parser.input[parser.index...parser.failure_index]
-        found = parser.input[parser.failure_index..parser.failure_index]
-        
+        expected = tf.size == 1 ? tf[0].expected_string.inspect : "one of #{tf.map{|f| f.expected_string}.uniq*', '}"
         line = parser.failure_line + line_offset
-        message = "#{file}:#{line}:#{parser.failure_column}: " +
-          "Parse error, expected #{expected}. After #{after.inspect}. Found: #{found.inspect}"
+        message = "#{file}:#{line}:#{parser.failure_column}: Parse error, expected #{expected}."
         super(message)
       end
     end
--- spec/cucumber/treetop_parser/scenario_outline.feature
+++ spec/cucumber/treetop_parser/scenario_outline.feature
@@ -4,7 +4,7 @@ Scenario Outline: Joe fails to login
   Given I login as Joe without the '<Privilege>' privilege
   When I <Request Method> /admin/<Path>
   Then I should see the text "Sorry Joe, you're not allowed to see <Path>"
-    
+  
   Examples:
   | Privilege | Request Method | Path       |
   | user      | GET            | reports    |
