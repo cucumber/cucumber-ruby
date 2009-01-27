1bbcdc7e64cfc908066884d04030e5012ef56971
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-01-27 23:53:06 +0100

    Make it possible to alias keywords in the grammar

--- lib/cucumber/parser.rb
+++ lib/cucumber/parser.rb
@@ -13,12 +13,30 @@ module Cucumber
   #
   # The AST classes are defined in the Cucumber::Ast module.
   module Parser
+    translator = Module.new do
+      def keywordz(key)
+        'Scenario'
+      end
+    end
+
     def self.load_parser(keywords)
-      template = File.open(File.dirname(__FILE__) + "/parser/i18n.tt", Cucumber.file_mode('r')).read
-      erb = ERB.new(template)
-      grammar = erb.result(binding)
-      Treetop.load_from_string(grammar)
-      require 'cucumber/parser/feature'
+      Loader.new(keywords)
+    end
+    
+    class Loader
+      def initialize(keywords)
+        @keywords = keywords
+        template = File.open(File.dirname(__FILE__) + "/parser/i18n.tt", Cucumber.file_mode('r')).read
+        erb = ERB.new(template)
+        grammar = erb.result(binding)
+        Treetop.load_from_string(grammar)
+        require 'cucumber/parser/feature'
+      end
+
+      def keywords(key)
+        values = @keywords[key].split('|')
+        values.map{|value| "'#{value}'"}.join(" / ")
+      end
     end
   end
 end
