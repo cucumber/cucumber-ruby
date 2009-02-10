7a7deca1b4c1d236f601ffab2a4cc121715e1f1d
Ben Mabey <ben@benmabey.com>
2009-02-10 16:01:01 -0700

    moving generated path_to method out of Object and into the World object

--- rails_generators/cucumber/templates/paths.rb
+++ rails_generators/cucumber/templates/paths.rb
@@ -1,12 +1,19 @@
-def path_to(page_name)
-  case page_name
-  
-  when /the homepage/i
-    root_path
-  
-  # Add more page name => path mappings here
-  
-  else
-    raise "Can't find mapping from \"#{page_name}\" to a path."
+module NavigationHelpers
+  def path_to(page_name)
+    case page_name
+    
+    when /the homepage/i
+      root_path
+    
+    # Add more page name => path mappings here
+    
+    else
+      raise "Can't find mapping from \"#{page_name}\" to a path."
+    end
   end
+end
+
+World do |world|
+  world.extend NavigationHelpers
+  world
 end
