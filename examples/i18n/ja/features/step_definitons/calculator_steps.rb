bdb77a43d0668ccecc7c8fbfeccaa32b088fa878
KAKUTANI Shintaro <shintaro@kakutani.com>
2009-02-23 19:44:23 +0900

    update japanese language support and make examples/i18n/ja works.

--- examples/i18n/ja/features/step_definitons/calculator_steps.rb
+++ examples/i18n/ja/features/step_definitons/calculator_steps.rb
@@ -15,10 +15,10 @@ Given "$n を入力" do |n|
   @calc.push n.to_i
 end
 
-When /(\w+) ボタンを押した/ do |op|
+When /(\w+) を押した/ do |op|
   @result = @calc.send op
 end
 
-Then /結果は (.*) を表示/ do |result|
+Then /(.*) を表示/ do |result|
   @result.should == result.to_f
 end
