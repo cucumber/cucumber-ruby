765467a3f55cf5e7ccb2974c65473c0e6032ae66
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-02-27 14:36:57 +0100

    Regenerated parser

--- lib/cucumber/ast/background.rb
+++ lib/cucumber/ast/background.rb
@@ -24,18 +24,19 @@ module Cucumber
         if first_scenario?(scenario)
           @first_scenario = scenario
           visitor.visit_background(self)
-        else
-          @first_scenario = nil
         end
       end
 
       def first_scenario?(scenario)
+        # If we're using 2 visitors, the 2nd pass will not be nil.
         @first_scenario.nil? || @first_scenario == scenario
       end
 
       def accept(visitor)
         visitor.visit_comment(@comment)
         visitor.visit_background_name(@keyword, "", file_colon_line(@line), source_indent(text_length))
+        # TODO: Use @first_scenario's background steps instead.
+        # We're currently vising steps that won't be used in any scenarios :-/ 
         visitor.visit_steps(@steps.step_invocations)
       end
 
