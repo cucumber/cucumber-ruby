3325a34a02608496febe48b508a69a8804d90809
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-03-03 12:28:52 +0000

    ='u' and native step definitions

--- examples/i18n/ru/features/step_definitons/calculator_steps.rb
+++ examples/i18n/ru/features/step_definitons/calculator_steps.rb
@@ -4,21 +4,14 @@ $:.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'cucumber/formatters/unicode'
 require 'calculator'
 
-Before do
-  @calc = Calculator.new
-end
-
-After do
-end
-
-Given /ввожу число (\d+)/ do |n|
+Допустим /ввожу число (\d+)/ do |n|
   @calc.push n.to_i
 end
 
-When /нажимаю "(.*)"/ do |op|
+Если /нажимаю "(.*)"/ do |op|
   @calc.send op
 end
 
-Then /должен увидеть на экране число (\d+)/ do |result|
+То /должен увидеть на экране число (\d+)/ do |result|
   @calc.result.should == result.to_f
 end
