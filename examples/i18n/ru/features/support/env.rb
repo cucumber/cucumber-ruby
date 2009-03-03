1cde7c023c7c43f0d165c4170793ee25dbe89000
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-03-03 13:33:40 +0000

    Slightly changed structure in Russian examples

--- examples/i18n/ru/features/support/env.rb
+++ examples/i18n/ru/features/support/env.rb
@@ -1,7 +1,6 @@
-$KCODE='u'
-
-World do  
-  def calc
-    @calc ||= Calculator.new
-  end
-end
+# encoding: utf-8
+require 'spec/expectations'
+$:.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'cucumber/formatters/unicode'
+require 'calculator'
+$KCODE='u' unless Cucumber::RUBY_1_9
