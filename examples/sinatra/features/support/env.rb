29d83d51907b8bfa455c1a2e365c72d77655e85c
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-03-21 11:50:25 -0400

    DRYness, pointer to wiki

--- examples/sinatra/features/support/env.rb
+++ examples/sinatra/features/support/env.rb
@@ -1,7 +1,11 @@
+# See http://wiki.github.com/aslakhellesoy/cucumber/sinatra
+# for more details about Sinatra with Cucumber
+
 # Sinatra
-require File.join(File.dirname(__FILE__), *%w[.. .. app])
+app_file = File.join(File.dirname(__FILE__), *%w[.. .. app.rb])
+require app_file
 # Force the application name because polyglot breaks the auto-detection logic.
-Sinatra::Application.app_file = File.join(File.dirname(__FILE__), *%w[.. .. app.rb])
+Sinatra::Application.app_file = app_file
 
 # RSpec
 require 'spec/expectations'
