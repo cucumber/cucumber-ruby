78ba010b4b7186675841fbd4cc57a60a50686221
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-04-06 10:32:20 +0200

    Explain that #path_to is only for mapping static names to static routes

--- rails_generators/cucumber/templates/paths.rb
+++ rails_generators/cucumber/templates/paths.rb
@@ -1,4 +1,16 @@
 module NavigationHelpers
+  # Maps a static name to a static route.
+  #
+  # This method is *not* designed to map from a dynamic name to a 
+  # dynamic route like <tt>post_comments_path(post)</tt>. For dynamic 
+  # routes like this you should *not* rely on #path_to, but write 
+  # your own step definitions instead. Example:
+  #
+  #   Given /I am on the comments page for the "(.+)" post/ |name|
+  #     post = Post.find_by_name(name)
+  #     visit post_comments_path(post)
+  #   end
+  #
   def path_to(page_name)
     case page_name
     
