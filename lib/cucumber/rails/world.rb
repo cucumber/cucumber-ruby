e326ca47239013ca68733c51cb9ec7c1e112f083
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-04-26 19:11:01 +0200

    Fix typo

--- lib/cucumber/rails/world.rb
+++ lib/cucumber/rails/world.rb
@@ -33,7 +33,7 @@ module Cucumber #:nodoc:
     def self.use_transactional_fixtures
 
       unless ::Rails.configuration.cache_classes
-        warn "WARNING: You have set Rail's config.cache_classes to false (most likely in config/environments/test.rb).  This setting is known to break Cucumber's use_transactional_fixtures method. Set config.cache_classes to true if you want to use transactional fixtures.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
+        warn "WARNING: You have set Rails' config.cache_classes to false (most likely in config/environments/test.rb).  This setting is known to break Cucumber's use_transactional_fixtures method. Set config.cache_classes to true if you want to use transactional fixtures.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
       end
 
       World.use_transactional_fixtures = true
