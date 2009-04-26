955d9e34818ee332d9da3b49085615b21085f94f
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-04-27 00:29:32 +0200

    Added a more helpful error message for Ruby 1.9. [#292 state:resolved]

--- lib/cucumber/rails/world.rb
+++ lib/cucumber/rails/world.rb
@@ -7,8 +7,14 @@ else
   require 'action_controller/test_process'
   require 'action_controller/integration'
 end
-require 'test/unit/testresult'
 
+begin
+  require 'test/unit/testresult'
+rescue LoadError => e
+  e.message << "\nYou must gem install test-unit. For more info see https://rspec.lighthouseapp.com/projects/16211/tickets/292"
+  e.message << "\nAlso make sure you have rack 1.0.0 or higher."
+  raise e
+end
 
 # So that Test::Unit doesn't launch at the end - makes it think it has already been run.
 Test::Unit.run = true if Test::Unit.respond_to?(:run=)
