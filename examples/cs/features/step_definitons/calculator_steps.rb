09d0e8ed6b31479ae326ef04ad895ff3b1506fcf
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2009-05-14 13:56:53 +0200

    Added some explanatary comments

--- examples/cs/features/step_definitons/calculator_steps.rb
+++ examples/cs/features/step_definitons/calculator_steps.rb
@@ -1,9 +1,9 @@
 require 'spec/expectations'
 $:.unshift(File.dirname(__FILE__) + '/../../lib') # This line is not needed in your own project
-require 'Calculator'
+require 'Calculator' # Calculator.dll
 
 Before do
-  @calc = Demo::Calculator.new
+  @calc = Demo::Calculator.new # A .NET class in Calculator.dll
 end
 
 Given "I have entered $n into the calculator" do |n|
