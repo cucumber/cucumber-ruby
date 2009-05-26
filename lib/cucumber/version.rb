561592cdc9aeb8b7bafa8ed785982070628e1d7a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-05-26 02:22:02 +0200

    Neuter RSpec

--- lib/cucumber/version.rb
+++ lib/cucumber/version.rb
@@ -3,7 +3,7 @@ module Cucumber #:nodoc:
     MAJOR = 0
     MINOR = 3
     TINY  = 7
-    PATCH = 4 # Set to nil for official release
+    PATCH = 5 # Set to nil for official release
 
     STRING = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
     STABLE_STRING = [MAJOR, MINOR, TINY].join('.')
--- rails_generators/cucumber/templates/cucumber_environment.rb
+++ rails_generators/cucumber/templates/cucumber_environment.rb
@@ -15,7 +15,7 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem "cucumber",    :lib => false, :version => ">=<%= Cucumber::VERSION::STABLE_STRING %>"
-config.gem "webrat",      :lib => false, :version => ">=0.4.4"  
-config.gem "rspec",       :lib => false, :version => ">=1.2.6"  
-config.gem "rspec-rails", :lib => false, :version => ">=1.2.6"  
+config.gem "cucumber",    :lib => false,        :version => ">=<%= Cucumber::VERSION::STABLE_STRING %>"
+config.gem "webrat",      :lib => false,        :version => ">=0.4.4"
+config.gem "rspec",       :lib => false,        :version => ">=1.2.6"
+config.gem "rspec-rails", :lib => 'spec/rails', :version => ">=1.2.6"
