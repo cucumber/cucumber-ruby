c6faec5e6d28c235e5d02e323332217af363a1d0
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-05-27 23:27:55 +0200

    RSpec's be_* matchers did not work in 0.3.9 and probably earlier versions. Now they do. Release 0.3.9.1

--- lib/cucumber/rails/rspec.rb
+++ lib/cucumber/rails/rspec.rb
@@ -2,7 +2,9 @@ require 'cucumber/rails/world'
 require 'spec/expectations'
 require 'spec/rails/matchers'
 
-Cucumber::Rails::World.class_eval do
-  include Spec::Matchers
-  include Spec::Rails::Matchers
+[Cucumber::Rails::World, ActionController::Integration::Session].each do |klass|
+  klass.class_eval do
+    include Spec::Matchers
+    include Spec::Rails::Matchers
+  end
 end
