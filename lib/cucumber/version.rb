f327c264a3069187e1eaf19049833708ffc18f77
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-05-27 11:24:01 +0200

    Release 0.3.8

--- lib/cucumber/version.rb
+++ lib/cucumber/version.rb
@@ -2,8 +2,8 @@ module Cucumber #:nodoc:
   class VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    TINY  = 7
-    PATCH = 5 # Set to nil for official release
+    TINY  = 8
+    PATCH = nil # Set to nil for official release
 
     STRING = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
     STABLE_STRING = [MAJOR, MINOR, TINY].join('.')
