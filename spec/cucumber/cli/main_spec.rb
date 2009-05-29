d7b1c045bcd393456fb3d31f94c588e66064af63
Ben Mabey <ben@benmabey.com>
2009-05-29 14:53:13 -0600

    whitespace

--- spec/cucumber/cli/main_spec.rb
+++ spec/cucumber/cli/main_spec.rb
@@ -15,7 +15,7 @@ module Cucumber
       end
 
       describe "verbose mode" do
-      
+
         before(:each) do
           @empty_feature = Ast::Feature.new(nil, Ast::Comment.new(''), Ast::Tags.new(2, []), "Feature", [])
           Dir.stub!(:[])
@@ -24,23 +24,23 @@ module Cucumber
         it "should show ruby files required" do
           @cli = Main.new(%w{--verbose --require example.rb}, @out)
           @cli.stub!(:require)
-        
+
           @cli.execute!(Object.new.extend(StepMother))
-        
+
           @out.string.should include('example.rb')
         end
-      
+
         it "should show feature files parsed" do
           @cli = Main.new(%w{--verbose example.feature}, @out)
           @cli.stub!(:require)
-        
+
           Parser::FeatureParser.stub!(:new).and_return(mock("feature parser", :parse_file => @empty_feature))
-          
+
           @cli.execute!(Object.new.extend(StepMother))
 
           @out.string.should include('example.feature')
         end
-      
+
       end
 
       describe "diffing" do
@@ -48,32 +48,32 @@ module Cucumber
         before :each do
           @configuration = mock('Configuration', :null_object => true)
           Configuration.should_receive(:new).and_return(@configuration)
-        
+
           @step_mother = mock('StepMother', :null_object => true)
-        
+
           @cli = Main.new(nil, @out)
         end
-      
+
         it "uses Spec Differ::Default when diff is enabled" do
           @configuration.should_receive(:diff_enabled?).and_return(true)
-        
+
           ::Spec::Expectations::Differs::Default.should_receive(:new)
-        
+
           @cli.execute!(@step_mother)
         end
-      
+
         it "does not use Spec Differ::Default when diff is disabled" do
           @configuration.should_receive(:diff_enabled?).and_return(false)
-        
+
           ::Spec::Expectations::Differs::Default.should_not_receive(:new)
-        
+
           @cli.execute!(@step_mother)
         end
-      
+
       end
 
       describe "--format with class" do
-     
+
        describe "in module" do
 
           it "should resolve each module until it gets Formatter class" do
@@ -90,7 +90,7 @@ module Cucumber
             cli.execute!(Object.new.extend(StepMother))
           end
 
-        end            
+        end
       end
     end
   end
