d32a3593f31199c0065825e37910b98ef2d0ea0d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-06-30 01:50:58 +0200

    Better names

--- lib/cucumber/webrat/table_locator.rb
+++ lib/cucumber/webrat/table_locator.rb
@@ -3,7 +3,10 @@ module Webrat
     def self.xpath_search
       ".//table"
     end
-    
+
+    # Converts this Table element into a 2-dimensional array of String where each cell
+    # represents the inner_html of the <td> and <th> elements. The number of columns is
+    # determined by the number of cells in the first row.
     def to_a
       col_count = nil
       Webrat::XML.css_search(@element, 'tr').map do |row|
@@ -45,7 +48,9 @@ module Webrat
         "Could not find table matching '#{@value}'"
       end
     end
-    
+
+    # Returns a Table element located by +id_or_selector+, which can
+    # be a DOM id or a CSS selector.
     def table_at(id_or_selector)
       TableLocator.new(@session, dom, id_or_selector).locate!
     end
