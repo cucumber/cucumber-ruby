04be7f09ad1ac60601ed4d7974e13d1c1b2ab8f0
Joakim Kolsjö <http://joakim.kolsjo@gmail-com/>
2009-07-30 21:41:34 +0200

    click_link_within(parent, link) webrat step

--- rails_generators/cucumber/templates/webrat_steps.rb
+++ rails_generators/cucumber/templates/webrat_steps.rb
@@ -19,6 +19,10 @@ When /^I follow "([^\"]*)"$/ do |link|
   click_link(link)
 end
 
+When /^I follow "([^\"]*)" within "([^\"]*)"$/ do |link, parent|
+  click_link_within(parent, link)
+end
+
 When /^I fill in "([^\"]*)" with "([^\"]*)"$/ do |field, value|
   fill_in(field, :with => value) 
 end
