c12794dff84c22fc5fae499d646886cfa6448fbf
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-08-24 14:27:44 -0500

    Make world_spec.rb pass [#356]

--- spec/cucumber/rails/world_spec.rb
+++ spec/cucumber/rails/world_spec.rb
@@ -5,6 +5,11 @@ describe "Rails world" do
 
   it "should run without Test::Unit.run defined" do
     require "mini_rails"
+
+    step_mother = Cucumber::StepMother.new
+    step_mother.load_natural_language('en')
+    rb = step_mother.load_programming_language('rb')
+
     require "cucumber/rails/world"
   end
 
