5311736e3e089a551b48c2e932cf79b1a3499fd9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-09-21 01:17:29 +0200

    Factor out active_record stuff to a separate file. Remove the Cucumber::Rails.use_transactional_fixtures method.

--- lib/cucumber/rails/world.rb
+++ lib/cucumber/rails/world.rb
@@ -8,55 +8,21 @@ end
 require 'cucumber/rails/test_unit'
 require 'cucumber/rails/action_controller'
 
-$__cucumber_toplevel = self
+unless ::Rails.configuration.cache_classes
+  warn "WARNING: You have set Rails' config.cache_classes to false (most likely in config/environments/cucumber.rb).  This setting is known to break Cucumber's use_transactional_fixtures method. Set config.cache_classes to true if you want to use transactional fixtures.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
+end
 
 module Cucumber #:nodoc:
   module Rails
-    # All scenarios will execute in the context of a new instance of Cucumber::Rails:World if this file 
-    # is loaded. This gives Step Definitions access to all the methods from Rails' ActionController::IntegrationTest
     class World < ActionController::IntegrationTest
-      if defined?(ActiveRecord::Base)
-        self.use_transactional_fixtures = false
-      else
-        def self.fixture_table_names; []; end # Workaround for projects that don't use ActiveRecord
-      end
-
       def initialize #:nodoc:
         @_result = Test::Unit::TestResult.new
       end
     end
-
-    def self.use_transactional_fixtures
-
-      unless ::Rails.configuration.cache_classes
-        warn "WARNING: You have set Rails' config.cache_classes to false (most likely in config/environments/test.rb).  This setting is known to break Cucumber's use_transactional_fixtures method. Set config.cache_classes to true if you want to use transactional fixtures.  For more information see https://rspec.lighthouseapp.com/projects/16211/tickets/165."
-      end
-
-      World.use_transactional_fixtures = true
-      if defined?(ActiveRecord::Base)
-        $__cucumber_toplevel.Before do
-          @__cucumber_ar_connection = ActiveRecord::Base.connection
-          if @__cucumber_ar_connection.respond_to?(:increment_open_transactions)
-            @__cucumber_ar_connection.increment_open_transactions
-          else
-            ActiveRecord::Base.__send__(:increment_open_transactions)
-          end
-          @__cucumber_ar_connection.begin_db_transaction
-          ActionMailer::Base.deliveries = [] if defined?(ActionMailer::Base)
-        end
-        
-        $__cucumber_toplevel.After do
-          @__cucumber_ar_connection.rollback_db_transaction
-          if @__cucumber_ar_connection.respond_to?(:decrement_open_transactions)
-            @__cucumber_ar_connection.decrement_open_transactions
-          else
-            ActiveRecord::Base.__send__(:decrement_open_transactions)
-          end
-        end
-      end
-    end
   end
 end
+
+require 'cucumber/rails/active_record'
 
 World do
   Cucumber::Rails::World.new
--- rails_generators/cucumber/templates/env.rb
+++ rails_generators/cucumber/templates/env.rb
@@ -6,10 +6,6 @@ require 'cucumber/rails/world'
 # Comment out the next line if you don't want Cucumber Unicode support
 require 'cucumber/formatter/unicode'
 
-# Comment out the next line if you don't want transactions to
-# open/roll back around each scenario
-Cucumber::Rails.use_transactional_fixtures
-
 require 'webrat'
 require 'cucumber/webrat/element_locator' # Lets you do table.diff!(element_at('#my_table_or_dl_or_ul_or_ol').to_table)
 
