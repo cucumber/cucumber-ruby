c531f4a69f0b1da91ce888230a98368196aed61b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-09-23 16:53:30 +0200

    Added back a way to globally turn off AR transactions. Added a @no-txn tag to turn off txns on per-scenario basis. Renamed @allow_rescue tag to @allow-rescue.

--- rails_generators/cucumber/templates/env.rb
+++ rails_generators/cucumber/templates/env.rb
@@ -3,6 +3,20 @@ ENV["RAILS_ENV"] ||= "cucumber"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
 
+# Whether or not to run each scenario within a database transaction.
+#
+# If you leave this to true, you can turn off traqnsactions on a
+# per-scenario basis, simply tagging it with @no-txn
+Cucumber::Rails::World.use_transactional_fixtures = true
+
+# Whether or not to allow Rails to rescue errors and render them on
+# an error page. Default is false, which will cause an error to be
+# raised.
+#
+# If you leave this to false, you can turn on Rails rescuing on a
+# per-scenario basis, simply tagging it with @allow-rescue
+ActionController::Base.allow_rescue = false
+
 # Comment out the next line if you don't want Cucumber Unicode support
 require 'cucumber/formatter/unicode'
 
