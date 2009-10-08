3905134cbd7ebb76c3f900fefb290a18018eaff6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2009-10-08 15:14:42 +0200

    Release 0.4.0.rc1

--- lib/cucumber/version.rb
+++ lib/cucumber/version.rb
@@ -1,9 +1,9 @@
 module Cucumber #:nodoc:
   class VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 3
-    TINY  = 104
-    PATCH = 1 # Set to nil for official release
+    MINOR = 4
+    TINY  = 0
+    PATCH = 'rc1' # Set to nil for official release
 
     STRING = [MAJOR, MINOR, TINY, PATCH].compact.join('.')
   end
