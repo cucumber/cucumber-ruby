8fe4c0f1cb7edf7932668f9ffba3da949ee1c59b
Matt Wynne <matt@mattwynne.net>
2009-12-14 16:41:20 +0000

    Wire protocol now passes table arguments as raw array of array of string, instead of JSON serialized

--- spec/cucumber/wire_support/wire_step_definition_spec.rb
+++ spec/cucumber/wire_support/wire_step_definition_spec.rb
@@ -8,7 +8,7 @@ module Cucumber
         it "should pass the raw table to the connection" do
           connection = mock('connection')
           step_definition = WireStepDefinition.new(connection, 'id' => 'the-id')
-          expected_args = ["a","b", %{[["1","2"],["3","4"]]}]
+          expected_args = ["a","b", [["1","2"],["3","4"]]]
           connection.should_receive(:invoke).with('the-id', expected_args)
           args = ["a","b"]
           args << Cucumber::Ast::Table.new([["1","2"],["3","4"]])
