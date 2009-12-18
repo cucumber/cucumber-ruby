180ecc8c5b66473b49e8bb671d114d52310337e4
Matt Wynne <matt@mattwynne.net>
2009-12-18 17:33:10 +0000

    Start creating concrete classes for wire protocol request handlers, cleaning up the hacks to create StepMatch with all the required parameters during a StepMatches request

--- lib/cucumber/wire_support/wire_protocol.rb
+++ lib/cucumber/wire_support/wire_protocol.rb
@@ -4,25 +4,42 @@ require 'cucumber/wire_support/request_handler'
 module Cucumber
   module WireSupport
     module WireProtocol
-      def step_matches(name_to_match, name_to_report)
-        @name_to_match, @name_to_report = name_to_match, name_to_report
-        make_request(:step_matches, :name_to_match => name_to_match) do
+      module Requests
+        class StepMatches < RequestHandler
+          def initialize(connection)
+            @connection = connection
+            super(connection, :step_matches)
+          end
+          
+          def execute(params)
+            @name_to_match = params[:name_to_match]
+            @name_to_report = params.delete(:name_to_report) # not part of the protocol message
+            super
+          end
+
           def handle_step_matches(params)
             params.map do |raw_step_match|
               step_definition = WireStepDefinition.new(@connection, raw_step_match)
               step_args = raw_step_match['args'].map do |raw_arg|
                 StepArgument.new(raw_arg['val'], raw_arg['pos'])
               end
-              @connection.step_match(step_definition, step_args) # convoluted!
+              step_match(step_definition, step_args) # convoluted!
             end
+          end
+          
+          private
+          
+          def step_match(step_definition, step_args)
+            StepMatch.new(step_definition, @name_to_match, @name_to_report, step_args)
           end
         end
       end
-
-      def step_match(step_definition, step_args)
-        StepMatch.new(step_definition, @name_to_match, @name_to_report, step_args)
-      end
       
+      def step_matches(name_to_match, name_to_report)
+        handler = Requests::StepMatches.new(self)
+        handler.execute :name_to_match => name_to_match, :name_to_report => name_to_report
+      end
+
       def snippet_text(step_keyword, step_name, multiline_arg_class_name)
         request_params = { :step_keyword => step_keyword, :step_name => step_name, :multiline_arg_class => multiline_arg_class_name }
 
