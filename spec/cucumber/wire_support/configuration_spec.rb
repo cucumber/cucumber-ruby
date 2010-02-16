9f37a97b51abd7342a9c58e952b82a8216b8402f
Matt Wynne <matt@mattwynne.net>
2010-02-16 23:51:15 +0000

    Add default timeouts to commonly-timing out wire protocol messages

--- spec/cucumber/wire_support/configuration_spec.rb
+++ spec/cucumber/wire_support/configuration_spec.rb
@@ -5,29 +5,46 @@ require 'tempfile'
 module Cucumber
   module WireSupport
     describe Configuration do
-      it "should read the hostname / port from the file" do
-        wire_file = Tempfile.new('wire')
-        wire_file << %q{
+      let(:wire_file) { Tempfile.new('wire') }
+      let(:config) { Configuration.new(wire_file.path) }
+      
+      def write_wire_file(contents)
+        wire_file << contents
+        wire_file.close
+      end
+      
+      it "reads the hostname / port from the file" do
+        write_wire_file %q{
           host: localhost
           port: 54321
         }
-        wire_file.close
-        config = Configuration.new(wire_file.path)
         config.host.should == 'localhost'
         config.port.should == 54321
       end
       
-      it "should read the timeout for a specific message" do
-        wire_file = Tempfile.new('wire')
-        wire_file << %q{
+      it "reads the timeout for a specific message" do
+        write_wire_file %q{
           host: localhost
           port: 54321
           timeout:
             invoke: 99
         }
-        wire_file.close
-        config = Configuration.new(wire_file.path)
-        config.timeout(:invoke).should == 99
+        config.timeout('invoke').should == 99
+      end
+      
+      describe "a wire file with no timeouts specified" do
+        before(:each) do
+          write_wire_file %q{
+            host: localhost
+            port: 54321
+          }
+        end
+        
+        %w(invoke begin_scenario end_scenario).each do |message|
+          it "sets the default timeout for '#{message}' to 120 seconds" do
+            config.timeout(message).should == 120
+          end
+        end
       end
     end
   end
