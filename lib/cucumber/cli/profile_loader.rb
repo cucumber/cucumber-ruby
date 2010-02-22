7585baa9b5d165cd2d61525df9dab544d3d19426
Nathaniel Haas <natehaas@gmail.com>
2010-02-22 00:35:44 -0700

    Tokenize using a shell-emulating parser
    Splitting a profile string by spaces is inconsistent with how the arguments would be parsed on the command line.

--- lib/cucumber/cli/profile_loader.rb
+++ lib/cucumber/cli/profile_loader.rb
@@ -22,7 +22,8 @@ Defined profiles in cucumber.yml:
         case(args_from_yml)
           when String
             raise YmlLoadError, "The '#{profile}' profile in cucumber.yml was blank.  Please define the command line arguments for the '#{profile}' profile in cucumber.yml.\n" if args_from_yml =~ /^\s*$/
-            args_from_yml = args_from_yml.split(' ')
+            require 'shellwords'
+            args_from_yml = args_from_yml.shellsplit
           when Array
             raise YmlLoadError, "The '#{profile}' profile in cucumber.yml was empty.  Please define the command line arguments for the '#{profile}' profile in cucumber.yml.\n" if args_from_yml.empty?
           else
