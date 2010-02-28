8fff3022b3d99ae45e765b6ff782e42ec2658e72
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2010-02-28 23:53:57 +0100

    Use German method names

--- examples/i18n/de/features/step_definitons/calculator_steps.rb
+++ examples/i18n/de/features/step_definitons/calculator_steps.rb
@@ -11,14 +11,14 @@ end
 After do
 end
 
-Given /ich habe (\d+) in den Taschenrechner eingegeben/ do |n|
+Gegebensei /ich habe (\d+) in den Taschenrechner eingegeben/ do |n|
   @calc.push n.to_i
 end
 
-When /ich (\w+) drücke/ do |op|
+Wenn /ich (\w+) drücke/ do |op|
   @result = @calc.send op
 end
 
-Then /sollte das Ergebniss auf dem Bildschirm (.*) sein/ do |result|
+Dann /sollte das Ergebniss auf dem Bildschirm (.*) sein/ do |result|
   @result.should == result.to_f
 end
