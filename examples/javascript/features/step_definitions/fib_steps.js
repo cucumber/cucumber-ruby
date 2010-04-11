c90792428a5dfd34d486ce076d81ba917dcb57f0
Joseph Wilk <joe@josephwilk.net>
2010-04-11 22:01:56 +0100

    Now its really running the tests, get the javascript code to actually do what the feature specifies.

--- examples/javascript/features/step_definitions/fib_steps.js
+++ examples/javascript/features/step_definitions/fib_steps.js
@@ -1,16 +1,28 @@
-fibonacci = function(n){
-	return Math.round(Math.pow((Math.sqrt(5) + 1) / 2, Math.abs(n)) / Math.sqrt(5)) * (n < 0 && n % 2 ? -1 : 1);
-};
+function fibanacci(n){
+  return n<2?n:fibanacci(n-1)+fibanacci(n-2);
+}
+
+var fibonacciSeries = function(topFibonacci) {
+  var result = Array();
+  var currentFibanacciValue = fibanacci(1);
+  var i = 2;
+  while(currentFibanacciValue < topFibonacci) {
+    result.push(currentFibanacciValue);
+    currentFibanacciValue  = fibanacci(i);
+    i++;
+  }
+  return result;
+}
 
 var fib_result;
 
 When(/^I ask Javascript to calculate fibonacci up to (\d+)$/, function(n){
-  fib_result = fibonacci(n);
+  fib_result = fibonacciSeries(n);
 });
 
-Then(/^it should give me (\[.*\])$/, function(n){
-  expected_result = fibonacci(n)
+Then(/^it should give me (\[.*\])$/, function(expected_result){
+  fib_result = fib_result.join(",");
   if(fib_result != expected_result){
-    throw fib_result + " is not equal to " + expected_result;
+    throw 'Expected <' + expected_result + "> but got <" + fib_result + ">";
   }
 });
