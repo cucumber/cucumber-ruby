59e01ddc5ffb90c63a61c4faaefe89de31ad66b4
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-07-01 23:00:20 +0200

    Minor fixes

--- examples/watir/features/search.feature
+++ examples/watir/features/search.feature
@@ -8,5 +8,5 @@ Feature: Search
     When I search for "cucumber github"
     Then I should see
       """
-      written in Ruby
+      GitHub
       """
--- examples/webrat/features/support/env.rb
+++ examples/webrat/features/support/env.rb
@@ -1,12 +1,12 @@
 require 'webrat'
-require 'spec'
+require 'rspec'
 
 Webrat.configure do |config|
   config.mode = :mechanize
 end
 
 class WebratWorld
-  include Spec::Matchers
+  include RSpec::Matchers
   include Webrat::Methods
   include Webrat::Matchers
 end
