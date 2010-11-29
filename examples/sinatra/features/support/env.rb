7aab3ce5940e03d1583b391a9c8415a10c1bf0d7
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-11-29 19:18:42 +0000

    Fix sinatra. Bump gherkin version.

--- examples/sinatra/features/support/env.rb
+++ examples/sinatra/features/support/env.rb
@@ -1,10 +1,7 @@
 # See http://wiki.github.com/aslakhellesoy/cucumber/sinatra
 # for more details about Sinatra with Cucumber
 
-app_file = File.join(File.dirname(__FILE__), *%w[.. .. app.rb])
-require app_file
-# Force the application name because polyglot breaks the auto-detection logic.
-Sinatra::Application.app_file = app_file
+require File.dirname(__FILE__) + '/../../app'
 
 begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations'; end
 require 'rack/test'
