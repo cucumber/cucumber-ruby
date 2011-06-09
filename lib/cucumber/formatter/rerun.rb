b81c5c1466b567e6e7da23228ec7442dbe7f683a
Matt Wynne <matt@mattwynne.net>
2011-06-09 09:39:10 +0100

    Change rerun formatter to flush output during a test run so you can tail it.

--- lib/cucumber/formatter/rerun.rb
+++ lib/cucumber/formatter/rerun.rb
@@ -21,16 +21,21 @@ module Cucumber
         @file_names = []
         @file_colon_lines = Hash.new{|h,k| h[k] = []}
       end
+      
+      def before_feature(*)
+        @lines = []
+        @file = nil
+      end
+      
+      def after_feature(*)
+        after_first_time do
+          @io.print ' '
+        end
+        @io.print "#{@file}:#{@lines.join(':')}"
+        @io.flush
+      end
 
       def after_features(features)
-        files = @file_names.uniq.map do |file|
-          lines = @file_colon_lines[file]
-          "#{file}:#{lines.join(':')}"
-        end
-        @io.puts files.join(' ')
-        
-        # Flusing output to rerun tempfile here...
-        @io.flush
         @io.close
       end
 
@@ -41,13 +46,20 @@ module Cucumber
       def after_feature_element(feature_element)
         if @rerun
           file, line = *feature_element.file_colon_line.split(':')
-          @file_colon_lines[file] << line
-          @file_names << file
+          @lines << line
+          @file = file
         end
       end
 
       def step_name(keyword, step_match, status, source_indent, background)
         @rerun = true if [:failed, :pending, :undefined].index(status)
+      end
+      
+    private
+    
+      def after_first_time
+        yield if @not_first_time
+        @not_first_time = true
       end
     end
   end
