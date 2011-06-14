fb62be15f6dc0cc555a9b367cecc8d2378c6d691
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-06-15 00:10:08 +0100

    Made a little progress on features/cucumber-features/core.feature:29

--- features/step_definitions/cucumber-features/core_steps.rb
+++ features/step_definitions/cucumber-features/core_steps.rb
@@ -10,13 +10,25 @@ end
 
 When /^Cucumber executes "([^"]*)" with these step mappings:$/ do |scenario_name, mappings|
   mapping_code = mappings.raw.map do |pattern, result|
-    "Given /#{pattern}/ do\nend\n"
+<<-EOF
+Given /#{pattern}/ do
+  #{result == 'passing' ? '' : 'raise "bang!"'}
+end
+EOF
   end.join("\n")
   write_file("features/step_definitions/some_stepdefs.rb", mapping_code)
 
-  run_simple "#{CUCUMBER} features/a_feature.feature --name '#{scenario_name}'"
+  run_simple "#{CUCUMBER} features/a_feature.feature --name '#{scenario_name}'", false
 end
 
 Then /^the scenario passes$/ do
-  assert_exiting_with 0
+  assert_exiting_with true
+end
+
+Then /^the scenario fails$/ do
+  assert_exiting_with false
+end
+
+Then /^the step "([^"]*)" is not executed$/ do |step_name|
+  pending # express the regexp above with the code you wish you had
 end
