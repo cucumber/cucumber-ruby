5c95da2fa18dd3f72c9d171ad64906816335b4b9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-06-15 20:41:21 +0100

    More elegant way to detect whether a step is run. This clutters up the generated files that go into aruba docs, but I'll create a workaround for that later.

--- features/step_definitions/cucumber-features/core_steps.rb
+++ features/step_definitions/cucumber-features/core_steps.rb
@@ -1,4 +1,11 @@
+require 'erb'
 CUCUMBER = File.dirname(__FILE__) + '/../../../bin/cucumber'
+
+World(Module.new do
+  def step_file(pattern)
+    pattern.gsub(/ /, '_') + '.step'
+  end
+end)
 
 Given /^a scenario "([^"]*)" with:$/ do |scenario_name, steps|
   write_file("features/a_feature.feature", <<-EOF)
@@ -10,11 +17,17 @@ end
 
 When /^Cucumber executes "([^"]*)" with these step mappings:$/ do |scenario_name, mappings|
   mapping_code = mappings.raw.map do |pattern, result|
-<<-EOF
-Given /#{pattern}/ do
-  #{result == 'passing' ? '' : 'raise "bang!"'}
+    erb = ERB.new(<<-EOF, nil, '-')
+Given /<%= pattern -%>/ do
+<% if result == 'passing' -%>
+  File.open("<%= step_file(pattern) %>", "w")
+<% else -%>
+  File.open("<%= step_file(pattern) %>", "w")
+  raise "bang!"
+<% end -%>
 end
-EOF
+    EOF
+    erb.result(binding)
   end.join("\n")
   write_file("features/step_definitions/some_stepdefs.rb", mapping_code)
 
@@ -29,7 +42,8 @@ Then /^the scenario fails$/ do
   assert_exiting_with false
 end
 
-Then /^the step "([^"]*)" is skipped$/ do |step_name|
-  @aruba_keep_ansi = true # TODO: Make it possible to pass a keep_ansi param to all_stdout in aruba
-  all_stdout.should include("\e[36m#{step_name}\e[90m")
+Then /^the step "([^"]*)" is skipped$/ do |pattern|
+  if File.exist?(File.join(current_dir, step_file(pattern)))
+    raise "#{pattern} was not skipped"
+  end
 end
