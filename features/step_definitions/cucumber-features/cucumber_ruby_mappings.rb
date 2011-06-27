3bf38e06ecbb83686153e4cde4bcbd704eed4996
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-06-27 23:29:04 +0100

    Adapt to changes introduced to accommodate for cucumber-jvm

--- features/step_definitions/cucumber-features/cucumber_ruby_mappings.rb
+++ features/step_definitions/cucumber-features/cucumber_ruby_mappings.rb
@@ -1,4 +1,16 @@
 module CucumberRubyMappings
+  def features_dir
+    'features'
+  end
+
+  def run_scenario(scenario_name)
+    run_simple "#{cucumber_bin} features/a_feature.feature --name '#{scenario_name}'", false
+  end
+
+  def run_feature
+    run_simple "#{cucumber_bin} features/a_feature.feature", false
+  end
+
   def cucumber_bin
     File.expand_path(File.dirname(__FILE__) + '/../../../bin/cucumber')
   end
@@ -126,6 +138,30 @@ end
 
 EOF
     write_file("features/step_definitions/calculator_mappings.rb", mapping_code)
+  end
+
+  def assert_passing_scenario
+    assert_partial_output("1 scenario (1 passed)", all_output)
+    assert_success true
+  end
+
+  def assert_failing_scenario
+    assert_partial_output("1 scenario (1 failed)", all_output)
+    assert_success false
+  end
+
+  def assert_pending_scenario
+    assert_partial_output("1 scenario (1 pending)", all_output)
+    assert_success true
+  end
+
+  def assert_undefined_scenario
+    assert_partial_output("1 scenario (1 undefined)", all_output)
+    assert_success true
+  end
+  
+  def failed_output
+    "failed"
   end
 end
 
