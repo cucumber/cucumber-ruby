98a4b3f9df0d95be01d56ea2a7ef5be776c2ae13
dtao <daniel.tao@gmail.com>
2011-09-23 09:30:54 -0700

    updated Array#rotate to exhibit (hopefully) identical behavior to standard library implementation from 1.9

--- lib/cucumber/core_ext/array.rb
+++ lib/cucumber/core_ext/array.rb
@@ -1,7 +1,8 @@
 class Array #:nodoc:
-  def rotate(n)
+  def rotate(n = 1)
     # Translate a negative index to a positive one.
-    n = length + n if n < 0
+    n += length while n < 0
+    n -= length while n >= length
     
     self[n, length - n] + self[0, n]
   end unless method_defined? :rotate
--- spec/cucumber/core_ext/array_spec.rb
+++ spec/cucumber/core_ext/array_spec.rb
@@ -5,20 +5,35 @@ describe Array do
 
   describe "#rotate" do
     before do
-      @array = [ :a, :b, :c ]
+      @array = [1, 2, 3]
     end
     
-    it "exhibits identical behavior to Array#rotate from Ruby > 1.9" do
-      @array.rotate(1).should == [ :b, :c, :a ]
-      @array.rotate(-1).should == [ :c, :a, :b ]
+    it "returns a new array by rotating, whose first element is the element at the specified index" do
+      @array.rotate(1).should == [2, 3, 1]
+    end
+
+    it "rotates by 1 if no index is specified" do
+      @array.rotate.should == [2, 3, 1]
+    end
+
+    it "rotates in the counter direction for negative indexes" do
+      @array.rotate(-1).should == [3, 1, 2]
+    end
+
+    it "loops around if the specified index exceeds the length of the array" do
+      @array.rotate(4).should == [2, 3, 1]
+    end
+
+    it "loops around if the specified index exceeds the negative length of the array" do
+      @array.rotate(-5).should == [2, 3, 1]
     end
 
     it "returns the same array if the specified index is 0" do
-      @array.rotate(0).should == [ :a, :b, :c ]
+      @array.rotate(0).should == [1, 2, 3]
     end
 
     it "does not throw up if the the specified index is the last index" do
-      @array.rotate(2).should == [ :c, :a, :b ]
+      @array.rotate(2).should == [3, 1, 2]
     end
 
     it "does not throw up for a single-element array" do
