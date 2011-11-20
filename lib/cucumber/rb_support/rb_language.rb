c0cc4172336d7a91760564f2ca74f4c74dbfac9a
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-11-20 16:54:23 +0000

    Remove dead code

--- lib/cucumber/rb_support/rb_language.rb
+++ lib/cucumber/rb_support/rb_language.rb
@@ -65,20 +65,6 @@ module Cucumber
         end
       end
 
-      # Gets called for each file under features (or whatever is overridden
-      # with --require).
-      def step_definitions_for(rb_file) # Looks Unused - Delete?
-        begin
-          require rb_file # This will cause self.add_step_definition and self.add_hook to be called from RbDsl
-          step_definitions
-        rescue LoadError => e
-          e.message << "\nFailed to load #{code_file}"
-          raise e
-        ensure
-          @step_definitions = nil
-        end
-      end
-      
       def step_matches(name_to_match, name_to_format)
         @step_definitions.map do |step_definition|
           if(arguments = step_definition.arguments_from(name_to_match))
