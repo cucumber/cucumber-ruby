00392c55b90777265789a8c7d3ec966952c182e7
Andrew Kiellor <akiellor@gmail.com>
2012-04-23 00:23:10 -0700

    regexp_argument_matcher_spec.rb:20 is pending under jruby.

--- spec/cucumber/rb_support/regexp_argument_matcher_spec.rb
+++ spec/cucumber/rb_support/regexp_argument_matcher_spec.rb
@@ -4,14 +4,18 @@ require 'cucumber/rb_support/regexp_argument_matcher'
 module Cucumber
   module RbSupport
     describe RegexpArgumentMatcher do
+      include RSpec::WorkInProgress
+
       it "should create 2 arguments" do
         arguments = RegexpArgumentMatcher.arguments_from(/I (\w+) (\w+)/, "I like fish")
         arguments.map{|argument| [argument.val, argument.offset]}.should == [["like", 2], ["fish", 7]]
       end
 
       it "should create 2 arguments when first group is optional" do
-        arguments = RegexpArgumentMatcher.arguments_from(/should( not)? be flashed '([^']*?)'$/, "I should be flashed 'Login failed.'")
-        arguments.map{|argument| [argument.val, argument.offset]}.should == [[nil, nil], ["Login failed.", 21]]
+        pending_under :java, "requires cucumber/gherkin >= ac42f51" do
+          arguments = RegexpArgumentMatcher.arguments_from(/should( not)? be flashed '([^']*?)'$/, "I should be flashed 'Login failed.'")
+          arguments.map{|argument| [argument.val, argument.offset]}.should == [[nil, nil], ["Login failed.", 21]]
+        end
       end
     end
   end
