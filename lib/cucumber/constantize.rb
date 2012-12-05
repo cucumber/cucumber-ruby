29ba6e5d06105d830754309c51b304b95009ff6b
Kevin Sylvestre <kevin@ksylvest.com>
2012-12-05 01:59:45 -0800

    Refactored the constantize function to use 'false' for searching the ancestors of a module

--- lib/cucumber/constantize.rb
+++ lib/cucumber/constantize.rb
@@ -9,7 +9,7 @@ module Cucumber
 
         constant = ::Object
         names.each do |name|
-          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+          constant = constant.const_defined?(name, false) ? constant.const_get(name, false) : constant.const_missing(name)
         end
         constant
       rescue NameError => e
