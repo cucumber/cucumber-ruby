c6778d8cd8668857430a708748831d2b87c945dc
Erik Michaels-Ober <sferik@gmail.com>
2013-02-19 12:34:15 -0800

    Update gem tasks to use MultiJSON

--- gem_tasks/downloads.rb
+++ gem_tasks/downloads.rb
@@ -1,7 +1,7 @@
-require 'json'
 require 'httparty'
+require 'multi_json'
 
 IO.read(File.dirname(__FILE__) + '/versions.txt').each_line do |version|
   json = HTTParty.get("http://rubygems.org/api/v1/downloads/cucumber-#{version.strip}.json")
-  puts JSON.parse(json.body)['version_downloads']
+  puts MultiJson.load(json.body)['version_downloads']
 end
