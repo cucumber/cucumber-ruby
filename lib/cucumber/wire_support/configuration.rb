4d55fa50a253af05fb3ffc1226db9ef3e3a0b82a
Mike Robertson <mikerobe@me.com>
2013-02-23 08:38:48 -0500

    add unix domain socket support (wire protocol)

--- lib/cucumber/wire_support/configuration.rb
+++ lib/cucumber/wire_support/configuration.rb
@@ -4,17 +4,28 @@ require 'erb'
 module Cucumber
   module WireSupport
     class Configuration
-      attr_reader :host, :port
+      attr_reader :host, :port, :unix
 
       def initialize(wire_file)
-        params = YAML.load(ERB.new(File.read(wire_file)).result)
+        if Hash === wire_file
+          params = wire_file.reduce({}) { |h,(k,v)| h[k.to_s] = v; h }
+        else
+          params = YAML.load(ERB.new(File.read(wire_file)).result)
+        end
+
         @host = params['host']
         @port = params['port']
+        @unix = params['unix'] if RUBY_PLATFORM !~ /mingw|mswin/
         @timeouts = DEFAULT_TIMEOUTS.merge(params['timeout'] || {})
       end
 
       def timeout(message = nil)
         return @timeouts[message.to_s] || 3
+      end
+
+      def to_s
+        return @unix if @unix
+        "#{@host}:#{@port}"
       end
 
       DEFAULT_TIMEOUTS = {
