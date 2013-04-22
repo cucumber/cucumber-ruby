86224badce465f9dceffd2eb4ddef08873104d06
Matt Wynne <matt@mattwynne.net>
2013-04-22 08:28:20 +0100

    Remove instance_exec patch for Ruby 1.8.x #371

--- lib/cucumber/core_ext/instance_exec.rb
+++ lib/cucumber/core_ext/instance_exec.rb
@@ -8,29 +8,6 @@ module Cucumber
 end
 
 class Object #:nodoc:
-  unless defined? instance_exec # 1.9
-    # http://eigenclass.org/hiki/bounded+space+instance_exec
-    module InstanceExecHelper #:nodoc:
-    end
-    include InstanceExecHelper
-    def instance_exec(*args, &block)
-      begin
-        old_critical, Thread.critical = Thread.critical, true
-        n = 0
-        n += 1 while respond_to?(mname="__instance_exec#{n}")
-        InstanceExecHelper.module_eval{ define_method(mname, &block) }
-      ensure
-        Thread.critical = old_critical
-      end
-      begin
-        ret = send(mname, *args)
-      ensure
-        InstanceExecHelper.module_eval{ remove_method(mname) } rescue nil
-      end
-      ret
-    end
-  end
-
   # TODO: Move most of this stuff out to an InstanceExecutor class.
   def cucumber_instance_exec(check_arity, pseudo_method, *args, &block)
     cucumber_run_with_backtrace_filtering(pseudo_method) do
