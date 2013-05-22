aebc6086c5633e437ec4ef00006c6441ce871636
Steve Tooke <steve@heavi.es>
2013-05-22 11:05:52 +0100

    Release 1.3.2

--- lib/cucumber/platform.rb
+++ lib/cucumber/platform.rb
@@ -4,7 +4,7 @@ require 'rbconfig'
 
 module Cucumber
 unless defined?(Cucumber::VERSION)
-  VERSION       = '1.3.1'
+  VERSION       = '1.3.2'
   BINARY        = File.expand_path(File.dirname(__FILE__) + '/../../bin/cucumber')
   LIBDIR        = File.expand_path(File.dirname(__FILE__) + '/../../lib')
   JRUBY         = defined?(JRUBY_VERSION)
