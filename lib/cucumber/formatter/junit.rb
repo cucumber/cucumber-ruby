ab00d824bd4a29b81b53f24e882ba659cc3c2840
Ragavan Ambighananthan <eelam.ragavan@gmail.com>
2013-07-02 13:17:37 +0100

    fix skipped scenarios appearing as failures

--- lib/cucumber/formatter/junit.rb
+++ lib/cucumber/formatter/junit.rb
@@ -127,21 +127,21 @@ module Cucumber
       private
 
       def build_testcase(duration, status, exception = nil, suffix = "")
-        @time += duration
+	@time += duration
         classname = @feature_name
         name = "#{@scenario}#{suffix}"
-        pending = [:pending, :undefined].include?(status)
-        passed = (status == :passed || (pending && !@options[:strict]))
-
-        @builder.testcase(:classname => classname, :name => name, :time => "%.6f" % duration) do
-          unless passed
+        pending = [:pending, :undefined].include?(status) && ( !@options[:strict])
+        passed = (status == :passed || pending)
+        
+	@builder.testcase(:classname => classname, :name => name, :time => "%.6f" % duration) do
+	  if( passed == false && status != :skipped) 
             @builder.failure(:message => "#{status.to_s} #{name}", :type => status.to_s) do
               @builder.cdata! @output
               @builder.cdata!(format_exception(exception)) if exception
             end
             @failures += 1
           end
-          if passed and (status == :skipped || pending)
+          if(status == :skipped || pending)
             @builder.skipped
             @skipped += 1
           end
