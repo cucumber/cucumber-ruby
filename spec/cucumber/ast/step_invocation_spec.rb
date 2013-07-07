c05a748ded6be0702f17c5a451d498614f7aa091
Steve Tooke <steve@heavi.es>
2013-07-07 10:00:10 +0100

    Whitespace cleanup

--- spec/cucumber/ast/step_invocation_spec.rb
+++ spec/cucumber/ast/step_invocation_spec.rb
@@ -3,43 +3,43 @@ require 'cucumber/ast/step_invocation'
 module Cucumber
   module Ast
     describe StepInvocation do
-		  let(:step_invocation) do
-			  matched_cells = []
-			  StepInvocation.new(stub, stub, stub, matched_cells)  
-			end
+      let(:step_invocation) do
+        matched_cells = []
+        StepInvocation.new(stub, stub, stub, matched_cells)
+      end
 
-			describe "filtering backtraces" do
-			  context "when disabled" do
-				  before { Cucumber.stub use_full_backtrace: true }
+      describe "filtering backtraces" do
+        context "when disabled" do
+          before { Cucumber.stub use_full_backtrace: true }
 
-				  it "return the exception unmodified" do
-					  exception = stub
-						step_invocation.filter_backtrace(exception).should == exception
-		   		end
-				end
+          it "return the exception unmodified" do
+            exception = stub
+            step_invocation.filter_backtrace(exception).should == exception
+           end
+        end
 
-				context "when enabled" do
-				  before { Cucumber.stub use_full_backtrace: false }
+        context "when enabled" do
+          before { Cucumber.stub use_full_backtrace: false }
 
-					it "removes lines with 'gems/' in the path" do
-					  original_backtrace = ['/foo/bar/gems/baz', '/path/to/my/file.rb']
-					  exception = StandardError.new
-						exception.set_backtrace(original_backtrace)
-						result = step_invocation.filter_backtrace(exception).backtrace
-						result.should == ['/path/to/my/file.rb']
-					end
+          it "removes lines with 'gems/' in the path" do
+            original_backtrace = ['/foo/bar/gems/baz', '/path/to/my/file.rb']
+            exception = StandardError.new
+            exception.set_backtrace(original_backtrace)
+            result = step_invocation.filter_backtrace(exception).backtrace
+            result.should == ['/path/to/my/file.rb']
+          end
 
-					it "removes lines with '.gem' in the path" do
-					  original_backtrace = ['/foo/bar/.gem/baz', '/path/to/my/file.rb']
-					  exception = StandardError.new
-						exception.set_backtrace(original_backtrace)
-						result = step_invocation.filter_backtrace(exception).backtrace
-						result.should == ['/path/to/my/file.rb']
-					end
-				end
-				
-			end
-		end
-	end
+          it "removes lines with '.gem' in the path" do
+            original_backtrace = ['/foo/bar/.gem/baz', '/path/to/my/file.rb']
+            exception = StandardError.new
+            exception.set_backtrace(original_backtrace)
+            result = step_invocation.filter_backtrace(exception).backtrace
+            result.should == ['/path/to/my/file.rb']
+          end
+        end
+
+      end
+    end
+  end
 end
 
