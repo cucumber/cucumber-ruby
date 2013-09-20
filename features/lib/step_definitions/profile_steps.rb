27d4be2ac9780166e839fdae52d00f711136c821
Jim van Musscher <jim.van.musscher@gmail.com>
2013-09-20 12:37:40 +0200

    Get the profile steps to work

--- features/lib/step_definitions/profile_steps.rb
+++ features/lib/step_definitions/profile_steps.rb
@@ -1,15 +1,15 @@
 Given /^the following profiles? (?:are|is) defined:$/ do |profiles|
-  create_file('cucumber.yml', profiles)
+  step 'a file named "cucumber.yml" with:', profiles
 end
 
 Then /^the (.*) profile should be used$/ do |profile|
-  last_stdout.should =~ /Using the #{profile} profile/
+  step 'the stdout should contain:', profile
 end
 
 Then /^exactly these files should be loaded:\s*(.*)$/ do |files|
-  last_stdout.scan(/^  \* (.*\.rb)$/).flatten.should == files.split(/,\s+/)
+  all_stdout.scan(/^  \* (.*\.rb)$/).flatten.should == files.split(/,\s+/)
 end
 
 Then /^exactly these features should be ran:\s*(.*)$/ do |files|
-  last_stdout.scan(/^  \* (.*\.feature)$/).flatten.should == files.split(/,\s+/)
+  all_stdout.scan(/^  \* (.*\.feature)$/).flatten.should == files.split(/,\s+/)
 end
