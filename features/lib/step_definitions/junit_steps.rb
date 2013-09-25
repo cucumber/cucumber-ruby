363223043889d9b028ab10d5a15bdd7f29f6f9da
Jim van Musscher <jim.van.musscher@gmail.com>
2013-09-25 20:54:41 +0200

    Get steps to pass

--- features/lib/step_definitions/junit_steps.rb
+++ features/lib/step_definitions/junit_steps.rb
@@ -3,4 +3,11 @@ Given(/^the tmp directory is empty$/) do
 end
 
 Then(/^"(.*?)" with junit duration "(.*?)" should contain$/) do |actual_file, duration, text|
+  actual = IO.read(current_dir + '/' + actual_file)
+  actual = replace_junit_duration(actual, duration)
+  actual.should == text
+end
+
+def replace_junit_duration(s, replacement)
+  s.gsub(/\d+\.\d\d+/m, replacement)
 end
