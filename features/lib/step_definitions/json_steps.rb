bcb3b65f5016b246ba032886508166a4bac7fac2
John Voloski <johnvoloski@cwi.com.br>
2014-05-15 12:57:04 -0300

    doing update from features's rspec's syntax

--- features/lib/step_definitions/json_steps.rb
+++ features/lib/step_definitions/json_steps.rb
@@ -2,6 +2,6 @@ Then /^it should (pass|fail) with JSON:$/ do |pass_fail, json|
   actual = normalise_json(MultiJson.load(all_stdout))
   expected = MultiJson.load(json)
 
-  actual.should == expected
+  expect(actual).to eq expected
   assert_success(pass_fail == 'pass')
 end
