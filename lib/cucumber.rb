fa5e848943df8bd063e88875dc41146a5c7135d1
Matt Wynne <matt@mattwynne.net>
2015-02-21 09:52:03 +0000

    Remove clutter

--- lib/cucumber.rb
+++ lib/cucumber.rb
@@ -1,6 +1,3 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 require 'yaml'
 require 'cucumber/platform'
 require 'cucumber/runtime'
