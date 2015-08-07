f2c0ef30436459fd8c62c6ab7755adbe66fb7a74
Matt Wynne <matt@mattwynne.net>
2015-08-07 06:48:45 -0400

    Use constants where possible in backtrace filter
    Closes #890

--- lib/cucumber/formatter/backtrace_filter.rb
+++ lib/cucumber/formatter/backtrace_filter.rb
@@ -3,25 +3,26 @@ require 'cucumber/platform'
 
 module Cucumber
   module Formatter
+    BACKTRACE_FILTER_PATTERNS = \
+      [/vendor\/rails|lib\/cucumber|bin\/cucumber:|lib\/rspec|gems\/|minitest|test\/unit|.gem\/ruby|lib\/ruby/]
+
+    if(::Cucumber::JRUBY)
+      BACKTRACE_FILTER_PATERNS << /org\/jruby/
+    end
+
     class BacktraceFilter
       def initialize(exception)
         @exception = exception
-        @backtrace_filter_patterns = \
-          [/vendor\/rails|lib\/cucumber|bin\/cucumber:|lib\/rspec|gems\/|minitest|test\/unit|.gem\/ruby|lib\/ruby/]
-
-        if(::Cucumber::JRUBY)
-          @backtrace_filter_patterns << /org\/jruby/
-        end
-
-        @pwd_pattern = /#{::Regexp.escape(::Dir.pwd)}\//m
       end
 
       def exception
         return @exception if ::Cucumber.use_full_backtrace
-        @exception.backtrace.each{|line| line.gsub!(@pwd_pattern, "./")}
+
+        pwd_pattern = /#{::Regexp.escape(::Dir.pwd)}\//m
+        @exception.backtrace.each { |line| line.gsub!(pwd_pattern, "./") }
 
         filtered = (@exception.backtrace || []).reject do |line|
-          @backtrace_filter_patterns.detect { |p| line =~ p }
+          BACKTRACE_FILTER_PATTERNS.detect { |p| line =~ p }
         end
 
         if ::ENV['CUCUMBER_TRUNCATE_OUTPUT']
