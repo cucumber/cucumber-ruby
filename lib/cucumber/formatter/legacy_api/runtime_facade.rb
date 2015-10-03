9974babb66691cdde984851c62b7f3a0edcf8cf7
Matt Wynne <matt@mattwynne.net>
2015-10-03 23:13:36 +0100

    Remove dead code

--- lib/cucumber/formatter/legacy_api/runtime_facade.rb
+++ lib/cucumber/formatter/legacy_api/runtime_facade.rb
@@ -25,10 +25,6 @@ module Cucumber
         def steps(status = nil)
           results.steps(status)
         end
-
-        def step_match(step_name, name_to_report=nil)
-          support_code.step_match(step_name, name_to_report)
-        end
       end
 
     end
