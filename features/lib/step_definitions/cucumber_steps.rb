7fddbed378e3ce482b2f7c070e1cd7376f1986a3
danascheider <dscheider@renewfinancial.com>
2016-05-15 15:11:12 -0700

    Move steps for retry filter into other file

--- features/lib/step_definitions/cucumber_steps.rb
+++ features/lib/step_definitions/cucumber_steps.rb
@@ -32,42 +32,6 @@ Given /^a step definition that looks like this:$/ do |string|
   create_step_definition { string }
 end
 
-Given /^a scenario "([^\"]*)" that fails once, then passes$/ do |name|
-  write_file "features/#{name}.feature",
-  <<-FEATURE 
-  Feature: #{name}
-    Scenario: #{name}
-      Given it fails once, then passes
-  FEATURE
-
-  write_file "features/step_defnitions/#{name}_steps.rb",
-  <<-STEPS
-  Given(/^it fails once, then passes$/) do 
-    $#{name.downcase} ||= 0
-    $#{name.downcase} += 1
-    expect($#{name.downcase}).to eql 2
-  end
-  STEPS
-end
-
-Given /^a scenario "([^\"]*)" that fails twice, then passes$/ do |name|
-  write_file "features/#{name}.feature",
-  <<-FEATURE
-  Feature: #{name}
-    Scenario: #{name}
-      Given it fails twice, then passes
-  FEATURE
-
-  write_file "features/step_definitions/#{name}_steps.rb",
-  <<-STEPS 
-  Given(/^it fails twice, then passes$/) do 
-    $#{name.downcase} ||= 0
-    $#{name.downcase} += 1
-    expect($#{name.downcase}).to eql 3
-  end
-  STEPS
-end
-
 Given /^a scenario "([^\"]*)" that passes$/ do |name|
   write_file "features/#{name}.feature",
   <<-FEATURE
