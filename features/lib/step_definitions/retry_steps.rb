653237451ff39ce019ba81164a9317111d546f0c
Matt Wynne <matt@mattwynne.net>
2016-07-02 12:34:16 +0100

    Finish off retry
    - changed the acceptance test to use more descriptive names for the test
      scenarios (I kept getting confused)
    - fixed a bug in the acceptance tests caused by our scenarios all running
      in one process, and using global variables (I added init scripts to
      reset them to zero at the start of a test run).
    - tidied up the docs a bit in the acceptance test
    - removed the @wip tag
    - added the retry filter into the filterchain in runtime
    
    There's a puzzle here with the output on the second scenario, with two
    retries. I've detailed what I think the summary should be, but there
    seems to be a passing scenario missing from the results totals.
    
    Did I make a mistake, or is the code still wrong?

--- features/lib/step_definitions/retry_steps.rb
+++ features/lib/step_definitions/retry_steps.rb
@@ -1,35 +1,54 @@
-Given /^a scenario "([^\"]*)" that fails once, then passes$/ do |name|
+Given /^a scenario "([^\"]*)" that fails once, then passes$/ do |full_name|
+  name = snake_case(full_name)
   write_file "features/#{name}.feature",
   <<-FEATURE
-  Feature: #{name}
-    Scenario: #{name}
+  Feature: #{full_name} feature
+    Scenario: #{full_name}
       Given it fails once, then passes
   FEATURE
 
   write_file "features/step_defnitions/#{name}_steps.rb",
   <<-STEPS
   Given(/^it fails once, then passes$/) do
-    $#{name.downcase} ||= 0
-    $#{name.downcase} += 1
-    expect($#{name.downcase}).to eql 2
+    $#{name} += 1
+    expect($#{name}).to be > 1
   end
   STEPS
+
+  write_file "features/support/#{name}_init.rb",
+  <<-INIT
+  $#{name} = 0
+  INIT
 end
 
-Given /^a scenario "([^\"]*)" that fails twice, then passes$/ do |name|
+Given /^a scenario "([^\"]*)" that fails twice, then passes$/ do |full_name|
+  name = snake_case(full_name)
   write_file "features/#{name}.feature",
   <<-FEATURE
-  Feature: #{name}
-    Scenario: #{name}
+  Feature: #{full_name} feature
+    Scenario: #{full_name}
       Given it fails twice, then passes
   FEATURE
 
   write_file "features/step_definitions/#{name}_steps.rb",
   <<-STEPS
   Given(/^it fails twice, then passes$/) do
-    $#{name.downcase} ||= 0
-    $#{name.downcase} += 1
-    expect($#{name.downcase}).to eql 3
+    $#{name} ||= 0
+    $#{name} += 1
+    expect($#{name}).to be > 2
   end
   STEPS
+
+  write_file "features/support/#{name}_init.rb",
+  <<-INIT
+  $#{name} = 0
+  INIT
 end
+
+module SnakeCase
+  def snake_case(name)
+    name.downcase.gsub(/\W/, '_')
+  end
+end
+
+World(SnakeCase)
--- lib/cucumber/runtime.rb
+++ lib/cucumber/runtime.rb
@@ -229,6 +229,7 @@ module Cucumber
         filters << Cucumber::Core::Test::LocationsFilter.new(filespecs.locations)
         filters << Filters::Randomizer.new(@configuration.seed) if @configuration.randomize?
         filters << Filters::Quit.new
+        filters << Filters::Retry.new(@configuration)
         # TODO: can we just use RbLanguages's step definitions directly?
         step_match_search = StepMatchSearch.new(@support_code.ruby.method(:step_matches), @configuration)
         filters << Filters::ActivateSteps.new(step_match_search, @configuration)
