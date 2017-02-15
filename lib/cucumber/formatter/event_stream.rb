1ec97e0895da0362218ef4d97c5c4a4ce91c72f6
Matt Wynne <matt@mattwynne.net>
2017-02-15 20:59:52 +0100

    Spike event stream formatter

--- lib/cucumber/formatter/event_stream.rb
+++ lib/cucumber/formatter/event_stream.rb
@@ -8,8 +8,19 @@ module Cucumber
         @config, @io = config, config.out_stream
         @series = SecureRandom.uuid
         write_event type: "start"
+        config.on_event :test_run_starting, -> (event) {
+          write_event \
+            type: "test-run-starting",
+            testCases: event.test_cases.map(&:location)
+        }
         config.on_event :test_case_finished, -> (event) {
-          write_event type: "test-case-finished", location: event.test_case.location, result: event.result.to_sym.to_s
+          write_event \
+            type: "test-case-finished", 
+            location: event.test_case.location,
+            result: {
+              status: event.result.to_sym.to_s
+              duration: event.result.duration
+            }
         }
       end
 
