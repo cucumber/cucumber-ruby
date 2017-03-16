657da3cd58eb2956efc0454e18d7aa5a338df659
Matt Wynne <matt@mattwynne.net>
2017-03-16 16:17:57 +0000

    Update event protocol based on latest discussions

--- lib/cucumber/formatter/event_stream.rb
+++ lib/cucumber/formatter/event_stream.rb
@@ -16,9 +16,6 @@ module Cucumber
 
         current_test_case = nil
 
-        # TODO: add protocol version, working directory
-        write_event type: "start"
-
         config.on_event :gherkin_source_read, -> (event) {
           write_event \
           type: "source",
@@ -30,26 +27,19 @@ module Cucumber
           }
         }
 
-        # TODO: instead of one message, emit a series of pickle events, one for each test case (including hooks as steps)
         config.on_event :test_run_starting, -> (event) {
+          write_event \
+            type: "test-run-started",
+            workingDirectory: Dir.pwd,
+            timestamp: Time.now.to_i
+
           event.test_cases.each { |test_case|
             write_event \
-              type: "pickle",
-              uri: test_case.location.file,
-              pickle: {
-                name: test_case.name,
-                steps: test_case.test_steps.map { |test_step|
-                  test_step_to_json(test_step, test_case.location.file)
-                },
-                tags: test_case.tags.map { |tag|
-                  {
-                    name: tag.name,
-                    location: location_to_json(tag.location, test_case.location.file)
-                  }
-                },
-                locations: [
-                  location_to_json(test_case.location, test_case.location.file)
-                ]
+              type: "test-case",
+              id: test_case_id(test_case),
+              sourceLocation: location_to_json(test_case.location),
+              steps: test_case.test_steps.map { |test_step|
+                test_step_to_json(test_case, test_step)
               }
           }
         }
@@ -57,33 +47,27 @@ module Cucumber
         config.on_event :test_case_starting, -> (event) {
           current_test_case = event.test_case # TODO: add this to the core step events so we don't have to cache it here
           write_event \
-            type: "test-case-starting",
-            location: event.test_case.location
+            type: "test-case-started",
+            id: test_case_id(event.test_case)
         }
 
         config.on_event :test_step_starting, -> (event) {
           write_event \
-            type: "test-step-starting",
-            index: current_test_case.test_steps.index(event.test_step),
-            testCase: {
-              location: current_test_case.location
-            }
+            type: "test-step-started",
+            id: test_step_id(current_test_case, event.test_step)
         }
 
         config.on_event :test_step_finished, -> (event) {
           write_event \
             type: "test-step-finished",
-            index: current_test_case.test_steps.index(event.test_step),
-            testCase: {
-              location: current_test_case.location
-            },
+            id: test_step_id(current_test_case, event.test_step),
             result: result_to_json(event.result)
         }
 
         config.on_event :test_case_finished, -> (event) {
           write_event \
             type: "test-case-finished", 
-            location: event.test_case.location,
+            id: test_case_id(event.test_case),
             result: result_to_json(event.result)
         }
 
@@ -110,33 +94,12 @@ module Cucumber
         data
       end
 
-      def location_to_json(location, parent_file)
-        result = { line: location.line }
-        result[:uri] = location.file if location.file != parent_file
-        result
-      end
-
-      # TODO: use a plain location with line / colon for gherkin steps, use URI as well for hooks
-      def test_step_to_json(test_step, parent_uri)
-        if hook?(test_step)
-          {
-            locations: [
-              location_to_json(test_step.action_location, parent_uri)
-            ]
-          }
-        else
-          {
-            text: test_step.name,
-            locations: [
-              location_to_json(test_step.source.last.location, parent_uri),
-              location_to_json(test_step.action_location, parent_uri)
-            ]
-          }
-        end
-      end
-
-      def hook?(test_step)
-        not test_step.source.last.respond_to?(:actual_keyword)
+      def test_step_to_json(test_case, test_step)
+        {
+          id: test_step_id(test_case, test_step),
+          actionLocation: location_to_json(test_step.action_location),
+          sourceLocation: location_to_json(test_step.source.last.location)
+        }
       end
 
       def open_socket(port, host)
@@ -145,6 +108,21 @@ module Cucumber
 
       def write_event(attributes)
         @io.puts attributes.to_json
+      end
+
+      def test_step_id(test_case, test_step)
+        {
+          testCaseId: test_case_id(test_case),
+          index: test_case.test_steps.index(test_step)
+        }
+      end
+
+      def test_case_id(test_case)
+        location_to_json(test_case.location)
+      end
+
+      def location_to_json(location)
+        { uri: location.file, line: location.line }
       end
     end
   end
