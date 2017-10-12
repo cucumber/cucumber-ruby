8bf065d2530704c4e2a81fae74ba662cec3b6e51
Alex Mikitenko <nonkor@gmail.com>
2017-10-11 18:52:42 -0700

    Fix html reporter with more than one embedded screenshot
    There was typo in #next_id: @indices used incorrect key for incrementing screenshot number (:type as a symbol instead of type as a variable)

--- spec/cucumber/formatter/html_spec.rb
+++ spec/cucumber/formatter/html_spec.rb
@@ -33,12 +33,15 @@ module Cucumber
       describe 'when writing the report to a file' do
         before(:each) do
           allow(@out).to receive(:respond_to?).with(:path).and_return(true)
-          expect(@out).to receive(:path).and_return('out/file.html')
-          run_defined_feature
-          @doc = Nokogiri.HTML(@out.string)
         end
 
         describe 'with a step that embeds a snapshot' do
+          before(:each) do
+            expect(@out).to receive(:path).and_return('out/file.html')
+            run_defined_feature
+            @doc = Nokogiri.HTML(@out.string)
+          end
+
           define_steps do
             Given(/snap/) {
               RSpec::Mocks.allow_message(File, :file?) { true }
@@ -54,6 +57,33 @@ module Cucumber
 
           it 'converts the snapshot path to a relative path' do
             expect(@doc.css('.embed img').first.attributes['src'].to_s).to eq 'snapshot.jpeg'
+          end
+        end
+
+        describe 'with more than one step that embeds snapshot' do
+          before(:each) do
+            expect(@out).to receive(:path).twice.and_return('out/file.html')
+            run_defined_feature
+            @doc = Nokogiri.HTML(@out.string)
+          end
+
+          define_steps do
+            Given(/snap/) {
+              RSpec::Mocks.allow_message(File, :file?) { true }
+              embed('out/snapshot.jpeg', 'image/jpeg')
+            }
+          end
+
+          define_feature(<<-FEATURE)
+          Feature:
+            Scenario:
+              Given snap
+              And snap
+            FEATURE
+
+          it 'specifies id for img incrementally' do
+            ids = @doc.css('img').map { |element| element.attributes['id'].to_s }
+            expect(ids).to match %w(img_1 img_2)
           end
         end
       end
@@ -364,7 +394,7 @@ module Cucumber
               Given log
             FEATURE
 
-          it { expect(@doc.at('a#text_0')['href'].to_s).to eq 'log.txt' }
+          it { expect(@doc.at('a#text_1')['href'].to_s).to eq 'log.txt' }
         end
 
         describe 'with a step that embeds a snapshot content manually' do
