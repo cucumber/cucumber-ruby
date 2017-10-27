b15e0fd2c11b4290a65b50c48f5f1021f9831ffd
Jayson Smith <github@nes.33mail.com>
2017-10-27 17:06:30 -0500

    Fix Style/AlignParameters

--- features/lib/step_definitions/retry_steps.rb
+++ features/lib/step_definitions/retry_steps.rb
@@ -2,14 +2,14 @@
 Given('a scenario {string} that fails once, then passes') do |full_name|
   name = snake_case(full_name)
   write_file "features/#{name}.feature",
-  <<-FEATURE
+             <<-FEATURE
   Feature: #{full_name} feature
     Scenario: #{full_name}
       Given it fails once, then passes
   FEATURE
 
   write_file "features/step_definitions/#{name}_steps.rb",
-  <<-STEPS
+             <<-STEPS
   Given(/^it fails once, then passes$/) do
     $#{name} += 1
     expect($#{name}).to be > 1
@@ -17,7 +17,7 @@ Given('a scenario {string} that fails once, then passes') do |full_name|
   STEPS
 
   write_file "features/support/#{name}_init.rb",
-  <<-INIT
+             <<-INIT
   $#{name} = 0
   INIT
 end
@@ -25,14 +25,14 @@ end
 Given('a scenario {string} that fails twice, then passes') do |full_name|
   name = snake_case(full_name)
   write_file "features/#{name}.feature",
-  <<-FEATURE
+             <<-FEATURE
   Feature: #{full_name} feature
     Scenario: #{full_name}
       Given it fails twice, then passes
   FEATURE
 
   write_file "features/step_definitions/#{name}_steps.rb",
-  <<-STEPS
+             <<-STEPS
   Given(/^it fails twice, then passes$/) do
     $#{name} ||= 0
     $#{name} += 1
@@ -41,7 +41,7 @@ Given('a scenario {string} that fails twice, then passes') do |full_name|
   STEPS
 
   write_file "features/support/#{name}_init.rb",
-  <<-INIT
+             <<-INIT
   $#{name} = 0
   INIT
 end
