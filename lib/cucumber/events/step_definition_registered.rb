68fe7292c65af935ca60348fb43db71f6a52f215
Jayson Smith <github@nes.33mail.com>
2018-01-23 20:12:07 -0700

    Fix Layout/LeadingCommentSpace auto

--- lib/cucumber/events/step_definition_registered.rb
+++ lib/cucumber/events/step_definition_registered.rb
@@ -14,7 +14,7 @@ module Cucumber
       # @return [RbSupport::RbStepDefinition]
       attr_reader :step_definition
 
-      #_@private
+      # _@private
       def initialize(step_definition)
         @step_definition = step_definition
       end
