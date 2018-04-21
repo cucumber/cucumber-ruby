c78e4654b79fd5e1a7d9fa3347e5c148e717b488
Larry Reaves <larry@yrral.net>
2018-04-21 14:56:08 -0400

    Make --fail-fast play nice with --retry (#1283)
    * Make --fail-fast play nice with --retry
    
    * added scenario to test retry with fail-fast
    
    * Reset Cucumber.wants_to_quit only in case of flaky tests.

--- lib/cucumber/formatter/fail_fast.rb
+++ lib/cucumber/formatter/fail_fast.rb
@@ -7,9 +7,15 @@ module Cucumber
   module Formatter
     class FailFast
       def initialize(configuration)
+        @previous_test_case = nil
         configuration.on_event :test_case_finished do |event|
-          _test_case, result = *event.attributes
-          Cucumber.wants_to_quit = true unless result.ok?(configuration.strict)
+          test_case, result = *event.attributes
+          if test_case != @previous_test_case
+            @previous_test_case = event.test_case
+            Cucumber.wants_to_quit = true unless result.ok?(configuration.strict)
+          elsif result.passed?
+            Cucumber.wants_to_quit = false
+          end
         end
       end
     end
