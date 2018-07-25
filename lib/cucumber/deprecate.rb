3c232616a1f00f2687edbd3ec196759b7b7edfee
Jayson Smith <github@nes.33mail.com>
2018-07-24 21:03:33 -0700

    Fix Performance/Caller

--- lib/cucumber/deprecate.rb
+++ lib/cucumber/deprecate.rb
@@ -9,7 +9,7 @@ module Cucumber
       AnsiEscapes = Cucumber::Gherkin::Formatter::AnsiEscapes
 
       def self.call(message, method, remove_after_version)
-        STDERR.puts AnsiEscapes.failed + "\nWARNING: ##{method} is deprecated and will be removed after version #{remove_after_version}. #{message}.\n(Called from #{caller[2]})" + AnsiEscapes.reset
+        STDERR.puts AnsiEscapes.failed + "\nWARNING: ##{method} is deprecated and will be removed after version #{remove_after_version}. #{message}.\n(Called from #{caller(3..3).first})" + AnsiEscapes.reset
       end
     end
 
