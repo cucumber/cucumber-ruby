d354a4a25f88b485795b6674a7921a0e28af80a5
Jason Schweier <jasons@nulogy.com>
2018-10-12 16:19:59 -0400

    Apply composed method to lower method complexity for RuboCop

--- lib/cucumber/cli/profile_loader.rb
+++ lib/cucumber/cli/profile_loader.rb
@@ -51,14 +51,29 @@ Defined profiles in cucumber.yml:
       # Loads the profile, processing it through ERB and YAML, and returns it as a hash.
       def cucumber_yml
         return @cucumber_yml if @cucumber_yml
-        unless cucumber_yml_defined?
-          raise(ProfilesNotDefinedError, "cucumber.yml was not found.  Current directory is #{Dir.pwd}." \
-          "Please refer to cucumber's documentation on defining profiles in cucumber.yml.  You must define" \
-          "a 'default' profile to use the cucumber command without any arguments.\nType 'cucumber --help' for usage.\n")
+
+        ensure_configuration_file_exists
+        process_configuration_file_with_erb
+        load_configuration
+
+        if @cucumber_yml.nil? || !@cucumber_yml.is_a?(Hash)
+          raise(YmlLoadError, 'cucumber.yml was found, but was blank or malformed. ' \
+          "Please refer to cucumber's documentation on correct profile usage.\n")
         end
 
+        @cucumber_yml
+      end
+
+      def ensure_configuration_file_exists
+        return if cucumber_yml_defined?
+
+        raise(ProfilesNotDefinedError, "cucumber.yml was not found.  Current directory is #{Dir.pwd}." \
+                                       "Please refer to cucumber's documentation on defining profiles in cucumber.yml.  You must define" \
+                                       "a 'default' profile to use the cucumber command without any arguments.\nType 'cucumber --help' for usage.\n")
+      end
+
+      def process_configuration_file_with_erb
         require 'erb'
-        require 'yaml'
         begin
           @cucumber_erb = if RUBY_VERSION >= '2.6'
                             ERB.new(IO.read(cucumber_file), trim_mode: '%').result(binding)
@@ -68,19 +83,15 @@ Defined profiles in cucumber.yml:
         rescue StandardError
           raise(YmlLoadError, "cucumber.yml was found, but could not be parsed with ERB.  Please refer to cucumber's documentation on correct profile usage.\n#{$ERROR_INFO.inspect}")
         end
+      end
 
+      def load_configuration
+        require 'yaml'
         begin
           @cucumber_yml = YAML.load(@cucumber_erb) # rubocop:disable Security/YAMLLoad
         rescue StandardError
           raise(YmlLoadError, "cucumber.yml was found, but could not be parsed. Please refer to cucumber's documentation on correct profile usage.\n")
         end
-
-        if @cucumber_yml.nil? || !@cucumber_yml.is_a?(Hash)
-          raise(YmlLoadError, 'cucumber.yml was found, but was blank or malformed. ' \
-          "Please refer to cucumber's documentation on correct profile usage.\n")
-        end
-
-        @cucumber_yml
       end
 
       # Locates cucumber.yml file. The file can end in .yml or .yaml,
