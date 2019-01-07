714f9d048abf2ac127ef98ff94fc0626e6e489df
Luke Hill <lukehill_uk@hotmail.com>
2019-01-07 12:19:32 +0100

    Optimise spec files
    Tidied up spec files to better conform to standards and improved read spec to output physical name of cucumber test

--- spec/cucumber/rake/task_spec.rb
+++ spec/cucumber/rake/task_spec.rb
@@ -7,13 +7,14 @@ require 'rake'
 module Cucumber
   module Rake
     describe Task do
-      describe '@task_name' do
-        context 'has read access to task name' do
-          it { expect(subject.respond_to? :task_name).to be true }
+      describe '#task_name' do
+        it 'can be read' do
+          expect(subject.task_name).to eq('cucumber')
         end
-        context 'has no write access to task name' do
-          it { expect(subject.respond_to? "#{:task_name}=").to be false }
-        end
+      end
+
+      describe '#task_name=' do
+        it { is_expected.not_to respond_to(:task_name=) }
       end
 
       describe '#cucumber_opts' do
