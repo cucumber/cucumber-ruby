ff43131d2d6f37e67dc876a864277ee92ed5b336
Vincent Pretre <vincent.pretre@gmail.com>
2019-12-03 16:39:36 +0100

    Use new signature for Core::Test::Case with pickle_id Use Core::Test::Case.to_envelope to generate Cucumber::Messages::TestCase

--- lib/cucumber/filters/broadcast_test_case_envelope.rb
+++ lib/cucumber/filters/broadcast_test_case_envelope.rb
@@ -5,23 +5,8 @@ module Cucumber
 
     class BroadcastTestCaseEnvelope < Core::Filter.new(:config)
       def test_case(test_case)
-        config.notify :envelope, Cucumber::Messages::Envelope.new(
-          testCase: Cucumber::Messages::TestCase.new(
-            id: test_case.id,
-            testSteps: test_case.test_steps.map { |step| test_step_to_message(step) }
-          )
-        )
+        config.notify :envelope, test_case.to_envelope
         self
-      end
-
-      private
-
-      def test_step_to_message(test_step)
-        Cucumber::Messages::TestCase::TestStep.new(
-          id: test_step.id,
-          hookId: test_step.hook? ? "123" : nil,
-          pickleStepId: test_step.hook? ? nil : "456"
-        )
       end
     end
   end
