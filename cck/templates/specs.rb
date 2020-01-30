ca502cddd21079276f7c9fa081be034fc1a7034d
Vincent Pretre <vincent.pretre@gmail.com>
2020-01-30 09:44:09 +0100

    CCK: use MONOREPO_SOURCE env var in shared examples

--- cck/templates/specs.rb
+++ cck/templates/specs.rb
@@ -2,10 +2,11 @@ require_relative '../../shared_examples.rb'
 
 describe __dir__ do
   let(:current_path) { __dir__ }
+  let(:monorepo_path) { ENV['MONOREPO_PATH'] || '../../../cucumber' }
   let(:examples_name) { File.basename(current_path) }
 
   let(:generated) { "#{current_path}/#{examples_name}.ndjson" }
-  let(:original) { "../../../cucumber/compatibility-kit/javascript/features/#{examples_name}/#{examples_name}.ndjson" }
+  let(:original) { "#{monorepo_path}/compatibility-kit/javascript/features/#{examples_name}/#{examples_name}.ndjson" }
 
   include_examples 'equivalent messages'
 end
