9ae6b6c2817af0b36d18d40658e08c6eb31a27d5
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-03 10:20:43 +0100

    Added Formatter::Query::StepDefinitionsByTestStep.step_match_arguments to find arguments for a step

--- spec/cucumber/formatter/query/step_definitions_by_test_step_spec.rb
+++ spec/cucumber/formatter/query/step_definitions_by_test_step_spec.rb
@@ -100,6 +100,62 @@ module Cucumber
               end
             end
           end
+
+          context '#step_match_arguments' do
+            context 'with a matching step without arguments' do
+              define_steps do
+                Given(/^there are bananas$/) {}
+              end
+
+              define_feature <<-FEATURE
+                Feature: Banana party
+
+                  Scenario: Monkey eats banana
+                    Given there are bananas
+              FEATURE
+
+              it 'returns an empty list' do
+                test_case = @test_cases.first
+                test_step = test_case.test_steps.first
+
+                expect(@formatter.step_match_arguments(test_step)).to be_empty
+              end
+            end
+
+            context 'with a matching step with arguments' do
+              define_steps do
+                Given(/^there are (.*)$/) {}
+              end
+
+              define_feature <<-FEATURE
+                Feature: Banana party
+
+                  Scenario: Monkey eats banana
+                    Given there are bananas
+              FEATURE
+
+              it 'returns an empty list' do
+                test_case = @test_cases.first
+                test_step = test_case.test_steps.first
+                matches = @formatter.step_match_arguments(test_step)
+
+                expect(matches.count).to eq(1)
+                expect(matches.first).to be_a(Cucumber::CucumberExpressions::Argument)
+                expect(matches.first.group.value).to eq('bananas')
+              end
+            end
+
+            context 'with an unknown step' do
+              define_feature 'Feature: Banana party'
+
+              it 'raises an exception' do
+                test_step = double
+                allow(test_step).to receive(:id).and_return('whatever-id')
+
+                expect { @formatter.step_match_arguments(test_step) }.to raise_error(Cucumber::Formatter::TestStepUnknownError)
+              end
+            end
+          end
         end
       end
     end
