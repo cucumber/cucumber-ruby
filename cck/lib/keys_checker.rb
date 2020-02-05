b3dd3b5cfda754ffcffbdf1d5c0fc479c25da5ad
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-05 11:48:05 +0100

    CCK: Do not raise errors with missing keys when default values are found in both expected and found message

--- cck/lib/keys_checker.rb
+++ cck/lib/keys_checker.rb
@@ -1,6 +1,10 @@
 module CCK
   class KeysChecker
     def self.compare(found, expected)
+      KeysChecker.new.compare(found, expected)
+    end
+
+    def compare(found, expected)
       errors = []
 
       found_keys = found.to_hash.keys
@@ -8,12 +12,25 @@ module CCK
 
       return errors if found_keys.sort == expected_keys.sort
 
-      missing_keys = expected_keys - found_keys
-      extra_keys = found_keys - expected_keys
+      default = default_values(found)
+
+      missing_keys = (expected_keys - found_keys).reject do |field_name|
+        default[field_name] == found[field_name] && default[field_name] == expected[field_name]
+      end
+
+      extra_keys = (found_keys - expected_keys).reject do |field_name|
+        default[field_name] == found[field_name] && default[field_name] == expected[field_name]
+      end
 
       errors << "Found extra keys in message #{found.class.name}: #{extra_keys}" unless extra_keys.empty?
       errors << "Missing keys in message #{found.class.name}: #{missing_keys}" unless missing_keys.empty?
       errors
+    end
+
+    def default_values(message)
+      default = {}
+      message.each_field { |field| default[field.name] = field.default_value }
+      default
     end
   end
 end
--- cck/spec/keys_checker_spec.rb
+++ cck/spec/keys_checker_spec.rb
@@ -3,6 +3,8 @@ require 'cucumber/messages'
 require_relative '../lib/keys_checker'
 
 describe CCK::KeysChecker do
+  let(:subject) { CCK::KeysChecker }
+
   describe '#compare' do
     let(:complete) do
       Cucumber::Messages::Duration.new(
@@ -31,26 +33,48 @@ describe CCK::KeysChecker do
     end
 
     it 'finds missing key' do
-      expect(CCK::KeysChecker.compare(missing_nanos, complete)).to eq(
+      expect(subject.compare(missing_nanos, complete)).to eq(
         ['Missing keys in message Cucumber::Messages::Duration: [:nanos]']
       )
     end
 
     it 'finds extra keys' do
-      expect(CCK::KeysChecker.compare(complete, missing_seconds)).to eq(
+      expect(subject.compare(complete, missing_seconds)).to eq(
         ['Found extra keys in message Cucumber::Messages::Duration: [:seconds]']
       )
     end
 
     it 'finds extra and missing' do
-      expect(CCK::KeysChecker.compare(missing_nanos, missing_seconds)).to contain_exactly(
+      expect(subject.compare(missing_nanos, missing_seconds)).to contain_exactly(
         'Missing keys in message Cucumber::Messages::Duration: [:nanos]',
         'Found extra keys in message Cucumber::Messages::Duration: [:seconds]'
       )
     end
 
     it 'does not care about the values' do
-      expect(CCK::KeysChecker.compare(complete, wrong_values)).to be_empty
+      expect(subject.compare(complete, wrong_values)).to be_empty
+    end
+
+    context 'when default values are omitted' do
+      # Depending on protobuf implementations, some default values may be omitted
+      # on JSON generation. So for once, we'll check the values, just in case.
+
+      let(:default_set) do
+        Cucumber::Messages::Duration.new(
+          seconds: 0,
+          nanos: 12
+        )
+      end
+
+      let(:default_not_set) do
+        Cucumber::Messages::Duration.new(
+          nanos: 12
+        )
+      end
+
+      it 'does not raise an exception' do
+        expect(subject.compare(default_set, default_not_set)).to be_empty
+      end
     end
   end
 end
