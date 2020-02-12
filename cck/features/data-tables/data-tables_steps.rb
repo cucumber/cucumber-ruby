07a8c20dead9112dbcdb3d450eb06e092f9ba3ca
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-12 17:33:51 +0100

    Use monorepo CCK for validation

--- cck/features/data-tables/data-tables_steps.rb
+++
@@ -1,7 +0,0 @@
-When('the following table is transposed:') do |table|
-  @transposed = table.transpose
-end
-
-Then('it should be:') do |expected|
-  @transposed.diff!(expected)
-end
--- cck/features/stack-traces/stack-traces_steps.rb
+++
@@ -1,3 +0,0 @@
-When('a step throws an exception') do
-  raise StandardError, 'An exception is raised here'
-end
