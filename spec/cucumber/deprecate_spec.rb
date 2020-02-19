5a3fe76ca7d1def174976f8bb44d71519c3edfcc
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-19 17:17:01 +0100

    Add some documentation for Deprecate and make it work again.
    I guess the way Cucumber::Gherkin::Formatter::AnsiEscapes works has changed a bit in 4 years
    and as we don't have tests for Cucumber.deprecate, this went unnoticed.

--- spec/cucumber/deprecate_spec.rb
+++ spec/cucumber/deprecate_spec.rb
@@ -3,6 +3,30 @@
 require 'spec_helper'
 
 module Cucumber::Deprecate
+  describe 'Cucumber.deprecate' do
+    context 'for developers' do
+      it 'fails when running the tests' do
+        expect do
+          Cucumber.deprecate('Use some_method instead', 'someMethod', '1.0.0')
+        end.to raise_exception('This method is due for removal after version 1.0.0')
+      end
+    end
+
+    context 'for users' do
+      it 'outputs a message to STDERR' do
+        stub_const('Cucumber::Deprecate::STRATEGY', ForUsers)
+        allow(STDERR).to receive(:puts)
+
+        Cucumber.deprecate('Use some_method instead', 'someMethod', '1.0.0')
+        expect(STDERR).to have_received(:puts).with(
+          a_string_including(
+            'WARNING: #someMethod is deprecated and will be removed after version 1.0.0. Use some_method instead.'
+          )
+        )
+      end
+    end
+  end
+
   describe CliOption do
     let(:error_stream) { double }
 
