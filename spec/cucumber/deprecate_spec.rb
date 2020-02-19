dd3a6181205bd0394acac05e6ebccc91468fbd93
Vincent Pretre <vincent.pretre@gmail.com>
2020-02-19 11:22:20 +0100

    Don't define useless subject

--- spec/cucumber/deprecate_spec.rb
+++ spec/cucumber/deprecate_spec.rb
@@ -4,13 +4,12 @@ require 'spec_helper'
 
 module Cucumber::Deprecate
   describe CliOption do
-    let(:subject) { Cucumber::Deprecate::CliOption }
     let(:error_stream) { double }
 
     context '.deprecate' do
       it 'outputs a warning to the provided channel' do
         allow(error_stream).to receive(:puts)
-        subject.deprecate(error_stream, '--some-option', 'Please use --another-option instead', '1.2.3')
+        described_class.deprecate(error_stream, '--some-option', 'Please use --another-option instead', '1.2.3')
 
         expect(error_stream).to have_received(:puts).with(
           a_string_including(
