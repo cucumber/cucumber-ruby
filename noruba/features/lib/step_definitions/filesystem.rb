32b9fcd11c48b2f6923646a1f7372641d7ece727
Vincent Pretre <vincent.pretre@gmail.com>
2020-05-06 08:55:19 +0200

    Try another approach for mkdir_p to see if this fixes the Windows issue with one scenario where the feature file appears to be missing.

--- noruba/features/lib/step_definitions/filesystem.rb
+++ noruba/features/lib/step_definitions/filesystem.rb
@@ -1,4 +1,12 @@
+def mkdir_p(path)
+  complete_path = '.'
+  path.split(/\/|\\/).each do |folder|
+    complete_path = File.join(complete_path, folder)
+    FileUtils.mkdir(complete_path) unless Dir.exist?(complete_path)
+  end
+end
+
 def write_file(path, content)
-  FileUtils.mkdir_p(File.dirname(path))
+  mkdir_p(File.dirname(path))
   File.open(path, 'w') { |file| file.write(content) }
 end
