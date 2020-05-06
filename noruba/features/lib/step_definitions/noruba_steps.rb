8679c71cdb032ef4f0fbf0dffeb3ec28e18af668
Vincent Pretre <vincent.pretre@gmail.com>
2020-05-06 15:58:06 +0200

    Keep on reorganizing steps

--- noruba/features/lib/step_definitions/noruba_steps.rb
+++ noruba/features/lib/step_definitions/noruba_steps.rb
@@ -48,22 +48,20 @@ Given('a directory without standard Cucumber project directory structure') do
 end
 
 Given('the standard step definitions') do
-  write_file 'features/step_definitions/steps.rb',
-             <<-STEPS
-  Given(/^this step passes$/)          { }
-  Given(/^this step raises an error$/) { raise 'error' }
-  Given(/^this step is pending$/)      { pending }
-  Given(/^this step fails$/)           { fail }
-  Given(/^this step is a table step$/) {|t| }
-  STEPS
+  write_file(
+    'features/step_definitions/steps.rb',
+    [
+      step_definition('/^this step passes$/', ''),
+      step_definition('/^this step raises an error$/', "raise 'error'"),
+      step_definition('/^this step is pending$/', 'pending'),
+      step_definition('/^this step fails$/', 'fail'),
+      step_definition('/^this step is a table step$/', '|t|')
+    ].join("\n")
+  )
 end
 
-Given('the following profiles are defined:') do |profiles|
+Given('the following profile(s) is/are defined:') do |profiles|
   write_file('cucumber.yml', profiles)
-end
-
-Given('the following profile is defined:') do |profile|
-  write_file('cucumber.yml', profile)
 end
 
 Given('log only formatter is declared') do
@@ -140,18 +138,21 @@ Given('a scenario {string} that fails once, then passes') do |full_name|
     )
   )
 
-  write_file "features/step_definitions/#{name}_steps.rb",
-             <<-STEPS
-  Given(/^it fails once, then passes$/) do
-    $#{name} += 1
-    expect($#{name}).to be > 1
-  end
-  STEPS
+  write_file(
+    "features/step_definitions/#{name}_steps.rb",
+    step_definition(
+      '/^it fails once, then passes$/',
+      [
+        "$#{name} += 1",
+        "expect($#{name}).to be > 1"
+      ].join("\n")
+    )
+  )
 
-  write_file "features/support/#{name}_init.rb",
-             <<-INIT
-  $#{name} = 0
-  INIT
+  write_file(
+    "features/support/#{name}_init.rb",
+    "  $#{name} = 0"
+  )
 end
 
 Given('a scenario {string} that fails twice, then passes') do |full_name|
@@ -164,19 +165,22 @@ Given('a scenario {string} that fails twice, then passes') do |full_name|
     )
   )
 
-  write_file "features/step_definitions/#{name}_steps.rb",
-             <<-STEPS
-  Given(/^it fails twice, then passes$/) do
-    $#{name} ||= 0
-    $#{name} += 1
-    expect($#{name}).to be > 2
-  end
-  STEPS
+  write_file(
+    "features/step_definitions/#{name}_steps.rb",
+    step_definition(
+      '/^it fails twice, then passes$/',
+      [
+        "$#{name} ||= 0",
+        "$#{name} += 1",
+        "expect($#{name}).to be > 2"
+      ].join("\n")
+    )
+  )
 
-  write_file "features/support/#{name}_init.rb",
-             <<-INIT
-  $#{name} = 0
-  INIT
+  write_file(
+    "features/support/#{name}_init.rb",
+    "  $#{name} = 0"
+  )
 end
 
 Given('a scenario {string} that passes') do |name|
@@ -188,10 +192,13 @@ Given('a scenario {string} that passes') do |name|
     )
   )
 
-  write_file "features/step_definitions/#{name}_steps.rb",
-             <<-STEPS
-  Given(/^it passes$/) { expect(true).to be true }
-  STEPS
+  write_file(
+    "features/step_definitions/#{name}_steps.rb",
+    step_definition(
+      '/^it passes$/',
+      'expect(true).to be true'
+    )
+  )
 end
 
 Given('a scenario {string} that fails') do |name|
@@ -203,10 +210,13 @@ Given('a scenario {string} that fails') do |name|
     )
   )
 
-  write_file "features/step_definitions/#{name}_steps.rb",
-             <<-STEPS
-  Given(/^it fails$/) { expect(false).to be true }
-  STEPS
+  write_file(
+    "features/step_definitions/#{name}_steps.rb",
+    step_definition(
+      '/^it fails$/',
+      'expect(false).to be true'
+    )
+  )
 end
 
 def snake_case(name)
---
+++ noruba/features/lib/support/step_definitions.rb
@@ -0,0 +1,24 @@
+module StepDefinitionsWorld
+  def step_definition(expression, content, keyword: 'Given')
+    if content.include?("\n")
+      block_step_definition(keyword, expression, content)
+    else
+      one_line_step_definition(keyword, expression, content)
+    end
+  end
+
+  def one_line_step_definition(keyword, expression, content)
+    "#{keyword}(#{expression}) { #{content} }"
+  end
+
+  def block_step_definition(keyword, expression, content)
+    indented_content = content
+      .split("\n")
+      .map { |line| "  #{line}"}
+      .join("\n")
+
+      "#{keyword}(#{expression}) do\n#{indented_content}\nend"
+  end
+end
+
+World(StepDefinitionsWorld)
