8e49672f37ae955c9d12b6159fd4783a48eff189
Vincent Pretre <vincent.pretre@gmail.com>
2020-05-06 09:12:20 +0200

    Undo previous tests, does not fix the issue

--- noruba/features/lib/step_definitions/filesystem.rb
+++ noruba/features/lib/step_definitions/filesystem.rb
@@ -1,12 +1,4 @@
-def mkdir_p(path)
-  complete_path = '.'
-  path.split(/\/|\\/).each do |folder|
-    complete_path = File.join(complete_path, folder)
-    FileUtils.mkdir(complete_path) unless Dir.exist?(complete_path)
-  end
-end
-
 def write_file(path, content)
-  mkdir_p(File.dirname(path))
+  FileUtils.mkdir_p(File.dirname(path))
   File.open(path, 'w') { |file| file.write(content) }
 end
