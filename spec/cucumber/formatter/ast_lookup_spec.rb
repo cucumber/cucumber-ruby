b7c1fd02d50e43e27f5d342baa87ab9b3c4070c4
Vincent Pretre <vincent.pretre@gmail.com>
2020-06-24 14:37:07 +0200

    Add ssome more test for AstLookup

--- spec/cucumber/formatter/ast_lookup_spec.rb
+++ spec/cucumber/formatter/ast_lookup_spec.rb
@@ -1,4 +1,87 @@
+require 'spec_helper'
+require 'cucumber/formatter/spec_helper'
 require 'cucumber/formatter/ast_lookup'
+require 'cucumber/cli/options'
+require 'json'
 
-describe 'AstLookup' do
+module Cucumber
+  module Formatter
+    describe AstLookup do
+      extend SpecHelperDsl
+      include SpecHelper
+
+      before(:each) do
+        @out = StringIO.new
+        @config = actual_runtime.configuration.with_options(out_stream: @out)
+        @formatter = AstLookup.new(@config)
+
+        @gherkin_documents = []
+        @test_cases = []
+
+        @config.on_event(:gherkin_source_parsed) do |event|
+          @gherkin_documents << event.gherkin_document
+        end
+
+        @config.on_event(:test_case_created) do |event|
+          @test_cases << event.test_case
+        end
+
+        run_defined_feature
+      end
+
+      describe 'gherkin_document(uri)' do
+        @feature = <<-FEATURE
+        Feature: some feature
+
+        Scenario: a simple scenario
+          Given a step
+        FEATURE
+        define_feature(@feature, 'path/to/the.feature')
+
+        it 'returns nil when no document match' do
+          expect(@formatter.gherkin_document('path/to/another.feature')).to be nil
+        end
+
+        it 'returns the matching Gherkin document' do
+          expect(@formatter.gherkin_document('path/to/the.feature')).to eq(@gherkin_documents.first)
+        end
+      end
+
+      context 'step_source(test_step)' do
+        @feature = <<-FEATURE
+        Feature: some feature
+
+        Scenario: a simple scenario
+          Given a step
+        FEATURE
+
+        define_feature(@feature, 'path/to/the.feature')
+
+        it 'returns the step' do
+          source = @formatter.step_source(@test_cases.first.test_steps.first)
+
+          expect(source.type).to eq(:Step)
+          expect(source.step).to eq(@gherkin_documents.first.feature.children.first.scenario.steps.first)
+        end
+      end
+
+      context 'snippet_step_keyword(test_step)' do
+        @feature = <<-FEATURE
+        Feature: some feature
+
+        Scenario: a simple scenario
+          Given a step
+          When another step
+          And a third step
+        FEATURE
+        define_feature(@feature, 'path/to/the.feature')
+
+        it 'returns the keyword for the snippet' do
+          keyword = @formatter.snippet_step_keyword(@test_cases.first.test_steps.last)
+
+          expect(keyword).to eq('When')
+        end
+      end
+    end
+  end
 end
