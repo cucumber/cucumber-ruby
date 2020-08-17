5ed5a6bd17f004ff926ebf2e191f92ae9d7dd71f
Christophe Bliard <christophe.bliard@trux.info>
2020-08-17 09:02:40 +0200

    Fix rubocop warnings

--- lib/cucumber/formatter/publish_banner_printer.rb
+++ lib/cucumber/formatter/publish_banner_printer.rb
@@ -9,43 +9,49 @@ module Cucumber
 
       def initialize(configuration)
         configuration.on_event :test_run_finished do |_event|
-          display_banner(
-            [
-              [
-                'Share your Cucumber Report with your team at ',
-                ['https://reports.cucumber.io', :cyan, :bold]
-              ],
-              '',
-              [
-                'Command line option:    ',
-                ['--publish', :cyan]
-              ],
-              [
-                'Environment variable:   ',
-                ['CUCUMBER_PLUGIN_PUBLISH_ENABLED=true', :cyan]
-              ],
-              '',
-              [
-                'More information at ',
-                ['https://reports.cucumber.io/docs/cucumber-ruby', :cyan]
-              ],
-              '',
-              [
-                'To disable this message, specify ',
-                ['CUCUMBER_PUBLISH_QUIET=true', :dark],
-                ' or use the '
-              ],
-              [
-                ['--publish-quiet', :dark],
-                ' option. You can also add this to your ',
-                ['cucumber.yml:', :dark]
-              ],
-              'default: --publish-quiet'
-            ],
-            configuration.error_stream
-          )
+          display_publish_ad(configuration.error_stream)
         end
       end
+
+      # rubocop:disable Metrics/MethodLength
+      def display_publish_ad(io)
+        display_banner(
+          [
+            [
+              'Share your Cucumber Report with your team at ',
+              ['https://reports.cucumber.io', :cyan, :bold]
+            ],
+            '',
+            [
+              'Command line option:    ',
+              ['--publish', :cyan]
+            ],
+            [
+              'Environment variable:   ',
+              ['CUCUMBER_PLUGIN_PUBLISH_ENABLED=true', :cyan]
+            ],
+            '',
+            [
+              'More information at ',
+              ['https://reports.cucumber.io/docs/cucumber-ruby', :cyan]
+            ],
+            '',
+            [
+              'To disable this message, specify ',
+              ['CUCUMBER_PUBLISH_QUIET=true', :dark],
+              ' or use the '
+            ],
+            [
+              ['--publish-quiet', :dark],
+              ' option. You can also add this to your ',
+              ['cucumber.yml:', :dark]
+            ],
+            'default: --publish-quiet'
+          ],
+          io
+        )
+      end
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
