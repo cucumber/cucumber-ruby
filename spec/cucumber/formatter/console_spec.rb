c6408d4da09ae2f748e10f146bce181f4d2c706d
Serghei Moret <serghei.moret@xing.com>
2021-05-30 21:49:07 +0200

    add tests

---
+++ spec/cucumber/formatter/console_spec.rb
@@ -0,0 +1,19 @@
+require 'cucumber/configuration'
+require 'cucumber/formatter/console'
+
+module Cucumber
+  module Formatter
+    describe Console do
+      include Console
+      it 'indents when padding is positive' do
+        res = indent('a line', 2)
+        expect(res).to eq '  a line'
+      end
+
+      it 'indents when padding is negative' do
+        res = indent('  a line', -2)
+        expect(res).to eq 'a line'
+      end
+    end
+  end
+end
