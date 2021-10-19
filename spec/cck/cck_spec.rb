5ba1d335e509341a7ea904eb7f62347aa5926cba
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-10-19 11:13:29 +0200

    Ignore messages issued from the CI itself

--- spec/cck/cck_spec.rb
+++ spec/cck/cck_spec.rb
@@ -14,6 +14,18 @@ class CCK::KeysChecker
   prepend KeysCheckerExtensions
 end
 
+module MessagesComparatorExtensions
+  def compare_message(found, expected)
+    return unless found.is_a?(Cucumber::Messages::Message)
+    return if found.is_a?(Cucumber::Messages::Ci) && expected.nil?
+    super(found, expected)
+  end
+end
+
+class CCK::MessagesComparator
+  prepend MessagesComparatorExtensions
+end
+
 describe 'Cucumber Compatibility Kit', cck: true do
   let(:cucumber_bin) { './bin/cucumber' }
   let(:cucumber_common_args) { '--publish-quiet --profile none --format message' }
