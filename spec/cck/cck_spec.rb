6271672011f5d707371d53f03e893daa59d90934
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-10-19 11:23:05 +0200

    Exclude extra Ci key when checking Meta message

--- spec/cck/cck_spec.rb
+++ spec/cck/cck_spec.rb
@@ -2,7 +2,20 @@ require 'cucumber-compatibility-kit'
 
 module KeysCheckerExtensions
   def compare(found, expected)
-    super(found, expected)
+    errors = []
+
+    found_keys = found.to_h(reject_nil_values: true).keys
+    expected_keys = expected.to_h(reject_nil_values: true).keys
+
+    return errors if found_keys.sort == expected_keys.sort
+
+    missing_keys = (expected_keys - found_keys)
+
+    extra_keys = (found_keys - expected_keys).reject { |key| ENV['CI'] && found.class == Cucumber::Messages::Meta && key ==  :ci }
+
+    errors << "Found extra keys in message #{found.class.name}: #{extra_keys}" unless extra_keys.empty?
+    errors << "Missing keys in message #{found.class.name}: #{missing_keys}" unless missing_keys.empty?
+    errors
   rescue StandardError => e
     puts "found: #{found.inspect}"
     puts "expected: #{expected.inspect}"
