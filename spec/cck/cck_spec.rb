e0dc2dd8f7dbf957b6373b528376d580798837e3
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-10-19 17:02:31 +0200

    Update CCK to 9.1.1

--- spec/cck/cck_spec.rb
+++ spec/cck/cck_spec.rb
@@ -1,44 +1,5 @@
 require 'cucumber-compatibility-kit'
 
-module KeysCheckerExtensions
-  def compare(found, expected)
-    errors = []
-
-    found_keys = found.to_h(reject_nil_values: true).keys
-    expected_keys = expected.to_h(reject_nil_values: true).keys
-
-    return errors if found_keys.sort == expected_keys.sort
-
-    missing_keys = (expected_keys - found_keys)
-
-    extra_keys = (found_keys - expected_keys).reject { |key| ENV['CI'] && found.class == Cucumber::Messages::Meta && key ==  :ci }
-
-    errors << "Found extra keys in message #{found.class.name}: #{extra_keys}" unless extra_keys.empty?
-    errors << "Missing keys in message #{found.class.name}: #{missing_keys}" unless missing_keys.empty?
-    errors
-  rescue StandardError => e
-    puts "found: #{found.inspect}"
-    puts "expected: #{expected.inspect}"
-    [e.message]
-  end
-end
-
-class CCK::KeysChecker
-  prepend KeysCheckerExtensions
-end
-
-module MessagesComparatorExtensions
-  def compare_message(found, expected)
-    return unless found.is_a?(Cucumber::Messages::Message)
-    return if found.is_a?(Cucumber::Messages::Ci) && expected.nil?
-    super(found, expected)
-  end
-end
-
-class CCK::MessagesComparator
-  prepend MessagesComparatorExtensions
-end
-
 describe 'Cucumber Compatibility Kit', cck: true do
   let(:cucumber_bin) { 'cucumber' }
   let(:cucumber_common_args) { '--publish-quiet --profile none --format message' }
