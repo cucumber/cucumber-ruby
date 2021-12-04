31c4aa9a4ea165996ff856953531c835f688c703
Evan Raffel <eraffel@mdsol.com>
2021-12-03 20:28:52 -0500

    Support docstrings in scneario examples in the JSON formatter

--- lib/cucumber/formatter/json.rb
+++ lib/cucumber/formatter/json.rb
@@ -176,15 +176,15 @@ module Cucumber
           name: test_step.text,
           line: test_step.location.lines.min
         }
-        step_hash[:doc_string] = create_doc_string_hash(step_source.doc_string) unless step_source.doc_string.nil?
+        step_hash[:doc_string] = create_doc_string_hash(step_source.doc_string, test_step.multiline_arg.content) unless step_source.doc_string.nil?
         step_hash[:rows] = create_data_table_value(step_source.data_table) unless step_source.data_table.nil?
         step_hash
       end
 
-      def create_doc_string_hash(doc_string)
+      def create_doc_string_hash(doc_string, doc_string_content)
         content_type = doc_string.media_type || ''
         {
-          value: doc_string.content,
+          value: doc_string_content,
           content_type: content_type,
           line: doc_string.location.line
         }
