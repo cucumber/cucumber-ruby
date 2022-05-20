b61e58a4a3d5ae2ba4977e3c68a015bcc854d9d0
Matt Wynne <matt@cucumber.io>
2022-05-20 12:59:33 -0700

    Fix renovate regex (and test)

--- scripts/renovate-test.js
+++ scripts/renovate-test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { readFileSync } = require('fs')
-const renovate = require('renovate.json')
-const regex = renovate.regexManagers.matchStrings
+const renovate = require(__dirname + '/../renovate.json')
+const regex = new RegExp(renovate.regexManagers[0].matchStrings)
 const lines = readFileSync('cucumber.gemspec', 'utf-8').split("\n").map(line => 
 	[line, (regex.exec(line) || { groups: "" }).groups]
 )
