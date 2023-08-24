6dbeb6f0a5975aadcd80f5979b488598303f96f2
Luke Hill <lukehill_uk@hotmail.com>
2023-08-24 11:29:11 +0100

    Remove unused files

--- gem_tasks/downloads.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'httparty'
-require 'json'
-
-IO.read("#{File.dirname(__FILE__)}/versions.txt").each_line do |version|
-  json = HTTParty.get("http://rubygems.org/api/v1/downloads/cucumber-#{version.strip}.json")
-  puts JSON.parse(json.body)['version_downloads']
-end
