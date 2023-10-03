055c7e6f731580fdf13ee9119f722c1ae9db7db0
Luke Hill <lukehill_uk@hotmail.com>
2023-10-03 12:09:27 +0100

    Remove all duplicate expectations from meta_message_builder

--- spec/cucumber/runtime/meta_message_builder_spec.rb
+++ spec/cucumber/runtime/meta_message_builder_spec.rb
@@ -4,39 +4,59 @@ require 'spec_helper'
 require 'cucumber/runtime/meta_message_builder'
 
 describe Cucumber::Runtime::MetaMessageBuilder do
-  describe 'self#build_meta_message' do
+  describe '.build_meta_message' do
     subject { described_class.build_meta_message }
 
     it { is_expected.to be_a(Cucumber::Messages::Meta) }
 
-    it 'fills system info in the meta message' do
+    it 'has a protocol version' do
       expect(subject.protocol_version).to eq(Cucumber::Messages::VERSION)
+    end
+
+    it 'has an implementation name' do
       expect(subject.implementation.name).to eq('cucumber-ruby')
+    end
+
+    it 'has an implementation version' do
       expect(subject.implementation.version).to eq(Cucumber::VERSION)
+    end
+
+    it 'has a runtime name' do
       expect(subject.runtime.name).to eq(RUBY_ENGINE)
+    end
+
+    it 'has a runtime version' do
       expect(subject.runtime.version).to eq(RUBY_VERSION)
+    end
+
+    it 'has an os name' do
       expect(subject.os.name).to eq(RbConfig::CONFIG['target_os'])
+    end
+
+    it 'has an os version' do
       expect(subject.os.version).to eq(Sys::Uname.uname.version)
+    end
+
+    it 'has a cpu name' do
       expect(subject.cpu.name).to eq(RbConfig::CONFIG['target_cpu'])
     end
 
-    context 'with overriden ENV' do
+    context 'with an overridden ENV hash' do
       subject { described_class.build_meta_message(env) }
 
       let(:env) { {} }
 
       it 'detects CI environment using the given env' do
         expect(Cucumber::CiEnvironment).to receive(:detect_ci_environment).with(env)
+
         subject
       end
     end
 
-    describe ':ci' do
+    describe ':ci hash' do
       subject { described_class.build_meta_message.ci }
 
-      before do
-        expect(Cucumber::CiEnvironment).to receive(:detect_ci_environment).and_return(ci_data)
-      end
+      before { expect(Cucumber::CiEnvironment).to receive(:detect_ci_environment).and_return(ci_data) }
 
       context 'when running on a CI system' do
         let(:ci_data) do
@@ -49,9 +69,15 @@ describe Cucumber::Runtime::MetaMessageBuilder do
 
         it { is_expected.to be_a(Cucumber::Messages::Ci) }
 
-        it 'fills ci data in the :ci field' do
+        it 'has a populated name from the ci input hash' do
           expect(subject.name).to eq(ci_data[:name])
+        end
+
+        it 'has a populated url from the ci input hash' do
           expect(subject.url).to eq(ci_data[:url])
+        end
+
+        it 'has a populated build number from the ci input hash' do
           expect(subject.build_number).to eq(ci_data[:buildNumber])
         end
 
@@ -72,15 +98,24 @@ describe Cucumber::Runtime::MetaMessageBuilder do
 
             it { is_expected.to be_a(Cucumber::Messages::Git) }
 
-            it 'fills the git data in the :git field' do
+            it 'has a populated git remote from the git field of the ci input hash' do
               expect(subject.remote).to eq(ci_data[:git][:remote])
+            end
+
+            it 'has a populated git revision from the git field of the ci input hash' do
               expect(subject.revision).to eq(ci_data[:git][:revision])
+            end
+
+            it 'has a populated git branch from the git field of the ci input hash' do
               expect(subject.branch).to eq(ci_data[:git][:branch])
+            end
+
+            it 'has a populated git tag from the git field of the ci input hash' do
               expect(subject.tag).to eq(ci_data[:git][:tag])
             end
           end
 
-          context 'without git data' do
+          context 'without any git data' do
             it { is_expected.to be_nil }
           end
         end
