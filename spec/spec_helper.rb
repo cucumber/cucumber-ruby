265a2704e4016a36d81636117e5f39a45b555178
Luke Hill <lukehill_uk@hotmail.com>
2023-11-10 11:52:12 +0000

    spec_helper fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,19 +7,5 @@ require 'simplecov_setup'
 require 'cucumber'
 
 RSpec.configure do |c|
-  c.before do
-    ::Cucumber::Term::ANSIColor.coloring = true
-  end
-end
-
-module RSpec
-  module WorkInProgress
-    def pending_under(platforms, reason, &block)
-      if [platforms].flatten.map(&:to_s).include? RUBY_PLATFORM
-        pending "pending under #{platforms.inspect} because: #{reason}", &block
-      else
-        yield
-      end
-    end
-  end
+  c.before { Cucumber::Term::ANSIColor.coloring = true }
 end
