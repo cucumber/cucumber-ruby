678ae5a028611d3810efeaf94660f0194fcaeaaf
Luke Hill <lukehill_uk@hotmail.com>
2024-01-04 17:17:12 +0000

    Calculate gherkin examples in gem to be ran locally

--- compatibility/cck_spec.rb
+++ compatibility/cck_spec.rb
@@ -3,10 +3,12 @@
 require_relative 'support/shared_examples'
 require_relative 'support/cck/examples'
 
+require 'cucumber-compatibility-kit'
+
 describe 'Cucumber Compatibility Kit', cck: true do
   let(:cucumber_command) { 'bundle exec cucumber --publish-quiet --profile none --format message' }
 
-  CCK::Examples.gherkin.each do |example_name|
+  Cucumber::CompatibilityKit.gherkin_examples.each do |example_name|
     describe "'#{example_name}' example" do
       include_examples 'cucumber compatibility kit' do
         let(:example) { example_name }
