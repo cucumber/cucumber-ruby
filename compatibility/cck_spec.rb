a09a058ddbf617b04919e14c6fa9175f1c2fe9dc
Luke Hill <lukehill_uk@hotmail.com>
2024-01-04 15:17:14 +0000

    Remove redundant variable assignment

--- compatibility/cck_spec.rb
+++ compatibility/cck_spec.rb
@@ -4,9 +4,7 @@ require_relative 'support/shared_examples'
 require_relative 'support/cck/compatibility_kit'
 
 describe 'Cucumber Compatibility Kit', cck: true do
-  let(:cucumber_bin) { 'cucumber' }
-  let(:cucumber_common_args) { '--publish-quiet --profile none --format message' }
-  let(:cucumber_command) { "bundle exec #{cucumber_bin} #{cucumber_common_args}" }
+  let(:cucumber_command) { 'bundle exec cucumber --publish-quiet --profile none --format message' }
 
   CCK::CompatibilityKit.gherkin_examples.each do |example_name|
     describe "'#{example_name}' example" do
