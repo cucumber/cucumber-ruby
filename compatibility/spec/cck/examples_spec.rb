acef611112955ea66423bc8cd2fc740fba7119da
Luke Hill <lukehill_uk@hotmail.com>
2024-01-05 10:16:58 +0000

    Copy, port and adapt meta-tests for CCK::Examples

---
+++ compatibility/spec/cck/examples_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../../support/cck/examples'
+
+describe CCK::Examples do
+  let(:features_path) { File.expand_path("#{File.dirname(__FILE__)}/../../features") }
+
+  describe '#supporting_code_for' do
+    context 'with an example that exists' do
+      it 'returns the path of the folder containing the supporting code for the example' do
+        expect(described_class.supporting_code_for('hooks')).to eq("#{features_path}/hooks")
+      end
+    end
+
+    context 'with an example that does not exist' do
+      it 'raises ArgumentError' do
+        expect { described_class.supporting_code_for('should-not-exist') }.to raise_error(ArgumentError)
+      end
+    end
+  end
+end
