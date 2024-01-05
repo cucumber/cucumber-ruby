f7155db631db1fbd83313e4c7bfc44eb08f4cb03
Luke Hill <lukehill_uk@hotmail.com>
2024-01-05 10:11:16 +0000

    Remove all feature example detection as that now lives in the gem proper

--- compatibility/support/cck/examples.rb
+++ compatibility/support/cck/examples.rb
@@ -3,26 +3,6 @@
 module CCK
   module Examples
     class << self
-      def all
-        gherkin + markdown
-      end
-
-      def gherkin
-        Dir.entries(features_folder_location).select do |file_or_folder|
-          next if file_or_folder.start_with?('.')
-
-          gherkin_example?(File.join(features_folder_location, file_or_folder))
-        end
-      end
-
-      def markdown
-        Dir.entries(features_folder_location).select do |file_or_folder|
-          next if file_or_folder.start_with?('.')
-
-          markdown_example?(File.join(features_folder_location, file_or_folder))
-        end
-      end
-
       def supporting_code_for(example_name)
         path = File.join(features_folder_location, example_name)
 
@@ -35,18 +15,6 @@ module CCK
 
       def features_folder_location
         File.expand_path("#{File.dirname(__FILE__)}/../../features/")
-      end
-
-      def gherkin_example?(example_folder)
-        file_type_in_folder?('.feature', example_folder)
-      end
-
-      def markdown_example?(example_folder)
-        file_type_in_folder?('.md', example_folder)
-      end
-
-      def file_type_in_folder?(extension, folder)
-        Dir.entries(folder).any? { |file| File.extname(file) == extension }
       end
     end
   end
