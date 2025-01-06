1649a8a9bbc3e83637f5a46c24fb27255a012fac
Luke Hill <lukehill_uk@hotmail.com>
2025-01-06 18:08:07 +0000

    The CCK expects a folder structure for each CCK feature - so we can add a blank dummy one for the empty feature

---
+++ compatibility/features/empty/empty_steps.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# The empty CCK scenario does not contain any executable steps. It contains just one scenario
+# which will be "ran" and only produce a single pickle for the name of the scenario (No pickles for steps)
