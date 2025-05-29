49b54ce787f8ef705ebeb06fd204fd1414e841cb
Luke Hill <lukehill_uk@hotmail.com>
2025-05-29 16:32:57 +0100

    Add CCK strawman for validating the testRunId consistency

--- compatibility/support/shared_examples.rb
+++ compatibility/support/shared_examples.rb
@@ -27,4 +27,18 @@ RSpec.shared_examples 'cucumber compatibility kit' do
 
     expect(comparator.errors).to be_empty, "There were comparison errors: #{comparator.errors}"
   end
+
+  it 'ensures a consistent `testRunStartedId` across the entire test run' do
+    # Step 1) Grab testRunStarted[:id]
+    test_run_message = parsed_generated.detect { |msg| message_type(msg) == :test_run_started }
+    id = test_run_message.test_run_started.id
+    
+    # Step 2) Validate every testCase has a [:testRunStartedId] equal to the above
+    # Step 3) Validate the single testRunFinished [:testRunStartedId] equal to the above
+    messages_types_containing_test_run_started_id = %i[test_case test_run_finished]
+    messages_containing_test_run_started_id = parsed_generated.select { |msg| messages_types_containing_test_run_started_id.include?(message_type(msg)) }
+    ids = messages_containing_test_run_started_id.map { |msg| msg.send(message_type(msg)).test_run_started_id }
+
+    expect(ids).to all eq(id)
+  end
 end
