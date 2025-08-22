f0012e29a7e8011a1d8e395c0db8c2b6ff162afd
Luke Hill <lukehill_uk@hotmail.com>
2025-08-22 20:14:49 +0100

    Fix up deprecation spec

--- spec/cucumber/cucumber_spec.rb
+++ spec/cucumber/cucumber_spec.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-
 module Cucumber
   describe '.deprecate' do
     it 'outputs a message to $stderr' do
-      allow($stderr).to receive(:puts)
+      allow(Kernel).to receive(:warn)
 
       Cucumber.deprecate('Use #some_other_method instead', '#some_method', '1.0.0')
-      expect($stderr).to have_received(:puts).with(
+      expect(Kernel).to have_received(:warn).with(
         a_string_including(
           'WARNING: #some_method is deprecated and will be removed after version 1.0.0. Use #some_other_method instead.'
         )
