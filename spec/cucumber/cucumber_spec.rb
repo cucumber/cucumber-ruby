a1e00ef30b9db45954daad78cd4a36c02e081217
Luke Hill <lukehill_uk@hotmail.com>
2025-08-22 20:18:47 +0100

    Rubocop fixes for deprecate class method spec

--- spec/cucumber/cucumber_spec.rb
+++ spec/cucumber/cucumber_spec.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
-module Cucumber
+describe Cucumber do
   describe '.deprecate' do
     it 'outputs a message to $stderr' do
       allow(Kernel).to receive(:warn)
 
-      Cucumber.deprecate('Use #some_other_method instead', '#some_method', '1.0.0')
-      expect(Kernel).to have_received(:warn).with(
-        a_string_including(
-          'WARNING: #some_method is deprecated and will be removed after version 1.0.0. Use #some_other_method instead.'
-        )
+      expect(Kernel).to receive(:warn).with(
+        a_string_including('WARNING: #some_method is deprecated and will be removed after version 1.0.0. Use #some_other_method instead.')
       )
+
+      described_class.deprecate('Use #some_other_method instead', '#some_method', '1.0.0')
     end
   end
 end
