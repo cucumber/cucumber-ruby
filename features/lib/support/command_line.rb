72479256a4afd05a5465f91ce16f10ce6b884a35
Luke Hill <lukehill_uk@hotmail.com>
2025-08-22 19:12:30 +0100

    Enhance stdout response from process to be formatted in a variety of ways

--- features/lib/support/command_line.rb
+++ features/lib/support/command_line.rb
@@ -30,8 +30,12 @@ class CommandLine
     @stderr.string
   end
 
-  def stdout
-    @stdout.string
+  def stdout(format: :string)
+    case format
+    when :lines; @stdout.string.split("\n")
+    when :messages; @stdout.string.split("\n").map { |line| JSON.parse(line) }
+    else @stdout.string
+    end
   end
 
   def all_output
