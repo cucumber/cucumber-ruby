ec538b8513035676e192f373504d2f778f0b300f
Luke Hill <lukehill_uk@hotmail.com>
2025-11-24 23:38:13 +0000

    Implement two more sub-update methods for generic update for hooks;

--- lib/cucumber/repository.rb
+++ lib/cucumber/repository.rb
@@ -10,18 +10,21 @@ module Cucumber
       return @test_run_started = envelope.test_run_started if envelope.test_run_started
       return @test_run_finished = envelope.test_run_finished if envelope.test_run_finished
 
-      # TODO: We need to improve these
+      # TODO: We will need to improve these over time
       return update_pickle(envelope.pickle) if envelope.pickle
       return update_test_case(envelope.test_case) if envelope.test_case
-      return update_test_case_started(envelope.test_case) if envelope.test_case
 
-      # Change all of the below lines to look like the above lines
-      #
-      # return @test_run_hook_started = envelope.test_run_hook_started if envelope.test_run_hook_started
-      # return @test_run_hook_finished = envelope.test_run_hook_finished if envelope.test_run_hook_finished
+      # TODO: These items are completed AS IS
+      return update_test_case_started(envelope.test_case) if envelope.test_case
+      return update_test_run_hook_started(envelope.test_case) if envelope.test_run_hook_started
+      return update_test_run_hook_finished(envelope.test_case) if envelope.test_run_hook_finished
+
+      # Change all the below lines to look like the above lines
       # return @test_case_finished = envelope.test_case_finished if envelope.test_case_finished
       # return @test_step_started = envelope.test_step_started if envelope.test_step_started
       # return @test_step_finished = envelope.test_step_finished if envelope.test_step_finished
+
+      nil
     end
 
     def pickle_by_id
@@ -35,6 +38,14 @@ module Cucumber
 
     def test_case_by_id
       @test_case_by_id ||= {}
+    end
+
+    def test_run_hook_started_by_id
+      @test_run_hook_started_by_id ||= {}
+    end
+
+    def test_run_hook_finished_by_test_run_hook_started_id
+      @test_run_hook_finished_by_test_run_hook_started_id ||= {}
     end
 
     # TODO: Not used yet
@@ -61,42 +72,18 @@ module Cucumber
     def update_test_case_started(test_case_started)
       test_case_started_by_id[test_case_started.id] = test_case_started
     end
+
+    def update_test_run_hook_started(test_run_hook_started)
+      test_run_hook_started_by_id[test_run_hook_started.id] = test_run_hook_started
+    end
+
+    def update_test_run_hook_finished(test_run_hook_finished)
+      test_run_hook_finished_by_test_run_hook_started_id[test_run_hook_finished.test_run_hook_started_id] = test_run_hook_finished
+    end
   end
 end
 
 ## Placeholder code that "might" be useful
-#    def update_gherkin_document(gherkin_document)
-#       :not_yet_implemented
-#     end
-#
-#     def update_test_case_started(test_case_started)
-#       @test_case_started_by_id[test_case_started.id] = test_case_started
-#
-#       # NOT YET IMPLEMENTED THE BELOW - NEXT STEPS from javascript implementation
-#       #     /*
-#       #     when a test case attempt starts besides the first one, clear all existing results
-#       #     and attachments for that test case, so we always report on the latest attempt
-#       #     (applies to legacy pickle-oriented query methods only)
-#       #      */
-#       #     const testCase = this.testCaseById.get(testCaseStarted.testCaseId)
-#       #     if (testCase) {
-#       #       this.testStepResultByPickleId.delete(testCase.pickleId)
-#       #       for (const testStep of testCase.testSteps) {
-#       #         this.testStepResultsByPickleStepId.delete(testStep.pickleStepId)
-#       #         this.testStepResultsbyTestStepId.delete(testStep.id)
-#       #         this.attachmentsByTestStepId.delete(testStep.id)
-#       #       }
-#       #     }
-#     end
-#
-#     def update_test_step_started(test_step_started)
-#       :not_yet_implemented
-#     end
-#
-#     def update_attachment(attachment)
-#       :not_yet_implemented
-#     end
-#
 #     def update_test_step_finished(test_step_finished)
 #       @test_step_finished_by_test_case_started_id[test_step_finished.test_case_started_id] = test_step_finished
 #
@@ -109,6 +96,3 @@ end
 #       #   }
 #     end
 #
-#     def update_test_case_finished(test_case_finished)
-#       :not_yet_implemented
-#     end
