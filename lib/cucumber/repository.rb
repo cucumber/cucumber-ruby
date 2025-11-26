fd0b88844a02f1498ce44e521057449cbeba9e11
Luke Hill <lukehill_uk@hotmail.com>
2025-11-26 21:37:41 +0000

    Fix alphabeticising by placing started before finished before each pair of update calls
    Add update_scenario which is a simple wrapper around updating steps

--- lib/cucumber/repository.rb
+++ lib/cucumber/repository.rb
@@ -27,13 +27,13 @@ module Cucumber
       return self.test_run_started = envelope.test_run_started if envelope.test_run_started
       return self.test_run_finished = envelope.test_run_finished if envelope.test_run_finished
       return update_gherkin_document(envelope.gherkin_document) if envelope.gherkin_document
+      return update_pickle(envelope.pickle) if envelope.pickle
       return update_test_run_hook_started(envelope.test_run_hook_started) if envelope.test_run_hook_started
       return update_test_run_hook_finished(envelope.test_run_hook_finished) if envelope.test_run_hook_finished
       return update_test_case_started(envelope.test_case_started) if envelope.test_case_started
       return update_test_case_finished(envelope.test_case_finished) if envelope.test_case_finished
       return update_test_step_started(envelope.test_step_started) if envelope.test_step_started
       return update_test_step_finished(envelope.test_step_finished) if envelope.test_step_finished
-      return update_pickle(envelope.pickle) if envelope.pickle
       return update_test_case(envelope.test_case) if envelope.test_case
 
       nil
@@ -81,33 +81,37 @@ module Cucumber
       pickle.steps.each { |pickle_step| pickle_step_by_id[pickle_step.id] = pickle_step }
     end
 
+    def update_scenario(scenario)
+      update_steps(scenario.steps)
+    end
+
     def update_test_case(test_case)
       test_case_by_id[test_case.id] = test_case
       test_case.test_steps.each { |test_step| test_step_by_id[test_step.id] = test_step }
-    end
-
-    def update_test_case_finished(test_case_finished)
-      test_case_finished_by_test_case_started_id[test_case_finished.test_case_started_id] = test_case_finished
     end
 
     def update_test_case_started(test_case_started)
       test_case_started_by_id[test_case_started.id] = test_case_started
     end
 
-    def update_test_run_hook_finished(test_run_hook_finished)
-      test_run_hook_finished_by_test_run_hook_started_id[test_run_hook_finished.test_run_hook_started_id] = test_run_hook_finished
+    def update_test_case_finished(test_case_finished)
+      test_case_finished_by_test_case_started_id[test_case_finished.test_case_started_id] = test_case_finished
     end
 
     def update_test_run_hook_started(test_run_hook_started)
       test_run_hook_started_by_id[test_run_hook_started.id] = test_run_hook_started
     end
 
-    def update_test_step_finished(test_step_finished)
-      test_steps_finished_by_test_case_started_id[test_step_finished.test_case_started_id] << test_step_finished
+    def update_test_run_hook_finished(test_run_hook_finished)
+      test_run_hook_finished_by_test_run_hook_started_id[test_run_hook_finished.test_run_hook_started_id] = test_run_hook_finished
     end
 
     def update_test_step_started(test_step_started)
       test_steps_started_by_test_case_started_id[test_step_started.test_case_started_id] << test_step_started
+    end
+
+    def update_test_step_finished(test_step_finished)
+      test_steps_finished_by_test_case_started_id[test_step_finished.test_case_started_id] << test_step_finished
     end
   end
 end
