910fb52a5c80dc8bdb6e53c3e4f633db954917b2
Luke Hill <lukehill_uk@hotmail.com>
2025-12-11 01:34:32 +0000

    Added all remaining step def code

--- compatibility/features/multiple-features-reversed/minimal_steps.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Given('I have {int} cukes in my belly') do |_cuke_count|
-  # no-op
-end
---
+++ compatibility/features/unused-steps/unused-steps_steps.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Given('a step that is used') do
+  # no-op
+end
+
+Given('a step that is not used') do
+  # no-op
+end
