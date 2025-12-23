808771da5b9eb2a78421538b79d6152cb4c762c6
Luke Hill <lukehill_uk@hotmail.com>
2025-12-23 17:30:36 +0000

    Remove placeholders and add in reporter

--- lib/cucumber/formatter/rerun_with_messages.rb
+++ lib/cucumber/formatter/rerun_with_messages.rb
@@ -20,44 +20,34 @@ module Cucumber
 
       # TODO: Fix this one method to make rerun formatter in new style
       def finish_report
-        # private void finishReport() {
-        #         query.findAllTestCaseStarted().stream()
-        #                 .filter(this::isNotPassingOrSkipped)
-        #                 .map(query::findPickleBy)
-        #                 .filter(Optional::isPresent) # get rid of nil values
-        #                 .map(Optional::get)  # get rid of nil values
-        #                 .map(this::createUriAndLine)
-        #                 .collect(groupByUriAndThenCollectLines())
-        #                 .forEach(this::printUriWithLines);
-        #         writer.close();
-        #     }
         @query.find_all_test_case_started.each do |test_case|
-          next unless not_passing_or_skipped?(test_case)
+          next if passing_or_skipped?(test_case)
 
           pickle = @query.find_pickle_by(test_case)
           next if pickle.nil?
 
           file = pickle.uri
           line = pickle.location
-          uri_and_line_store[file] << line
+          uri_and_location_hash[file] << line
 
+          # TODO: This outputs my desired failures in an array format. Now just to format to a file
           failure_array
         end
       end
 
       def failure_array
-        uri_and_line_store.map do |uri, lines|
+        uri_and_location_hash.map do |uri, lines|
           "#{uri}:#{lines.join(':')}"
         end
       end
 
-      def uri_and_line_store
-        @uri_and_line_store ||= Hash.new { |hash, key| hash[key] = [] }
+      def uri_and_location_hash
+        @uri_and_location_hash ||= Hash.new { |hash, key| hash[key] = [] }
       end
 
-      def not_passing_or_skipped?(test_case_started)
+      def passing_or_skipped?(test_case_started)
         most_severe_test_step_result = @query.find_most_severe_test_step_result_by(test_case_started)
-        ![TestStepResultStatus::PASSED, TestStepResultStatus::SKIPPED].include?(most_severe_test_step_result.status)
+        [TestStepResultStatus::PASSED, TestStepResultStatus::SKIPPED].include?(most_severe_test_step_result.status)
       end
     end
   end
