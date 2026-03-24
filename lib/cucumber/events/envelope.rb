d2110cbc6aeac7c70528135c7be57d0540c3605c
Luke Hill <lukehill_uk@hotmail.com>
2026-03-24 19:09:59 +0000

    Add new logic for easier debugging of envelope events

--- lib/cucumber/events/envelope.rb
+++ lib/cucumber/events/envelope.rb
@@ -6,6 +6,24 @@ module Cucumber
   module Events
     class Envelope < Core::Event.new(:envelope)
       attr_reader :envelope
+
+      def inspect
+        "Envelope Event -> Message Type: #{not_nil_message_name}}"
+      end
+
+      def to_s
+        inspect
+      end
+
+      private
+
+      def not_nil_message_name
+        envelope.instance_variables.detect { |message| !envelope.send(name_of(message)).nil? }
+      end
+
+      def name_of(message)
+        message.to_s.delete('@')
+      end
     end
   end
 end
