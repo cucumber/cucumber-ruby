2283c1af8154d1557fff5ea6f2b13872de3435d1
Luke Hill <20105237+luke-hill@users.noreply.github.com>
2026-04-07 00:57:55 +0100

    Refactor/remove redundant constants (#1845)
    * Split cucumber rake task out into composite files and reduce the scope of requirements
    
    * Fix spec files for rake specs
    
    * Recreate JRUBY and WINDOWS constants in cucumber top level namespace

--- lib/cucumber/platform.rb
+++ lib/cucumber/platform.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 require 'rbconfig'
-require 'cucumber/core/platform'
 
 module Cucumber
   VERSION       = File.read(File.expand_path('../../VERSION', __dir__)).strip
   BINARY        = File.expand_path("#{File.dirname(__FILE__)}/../../bin/cucumber")
-  LIBDIR        = File.expand_path("#{File.dirname(__FILE__)}/../../lib")
   RUBY_BINARY   = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
+  JRUBY         = defined?(JRUBY_VERSION)
+  WINDOWS       = RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
 
   class << self
     attr_writer :use_full_backtrace
