ecd1b01e6c991ba221de6374cd7ed4950da4c85e
Luke Hill <lukehill_uk@hotmail.com>
2026-04-15 19:17:27 +0100

    WIP: Use a little bit of refactoring to tidy up items

--- lib/cucumber/events/envelope.rb
+++ lib/cucumber/events/envelope.rb
@@ -4,54 +4,32 @@ require 'cucumber/core/events'
 
 module Cucumber
   module Events
-    class Envelope < Core::Event.new(:envelope)
+    class Envelope
       attr_reader :envelope
 
       # @return [Symbol] the underscored name of the class to be used as the key in an event registry
       def self.event_id
-        underscore(name.split('::').last).to_sym
+        :envelope
       end
 
-      def self.underscore(string)
-        string
-          .to_s
-          .gsub('::', '/')
-          .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-          .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-          .tr('-', '_')
-          .downcase
+      def initialize(envelope)
+        @envelope = envelope
       end
 
-      def self.new(*events)
-        # Use normal constructor for subclasses of Event
-        return super if ancestors.index(Event).positive?
+      # Here just is an array of each method defined as your readers
+      def attributes
+        [envelope]
+        to_h.map { |_k, v| v }
+      end
 
-        Class.new(Event) do
-          # NB: We need to use metaprogramming here instead of direct variable obtainment
-          # because JRuby does not guarantee the order in which variables are defined is equivalent
-          # to the order in which they are obtainable
-          #
-          # See https://github.com/jruby/jruby/issues/7988 for more info
-          attr_reader(*events)
+      def to_h
+        {
+          envelope: envelope
+        }
+      end
 
-          define_method(:initialize) do |*attributes|
-            events.zip(attributes) do |name, value|
-              instance_variable_set(:"@#{name}", value)
-            end
-          end
-
-          define_method(:attributes) do
-            events.map { |var| instance_variable_get(:"@#{var}") }
-          end
-
-          define_method(:to_h) do
-            events.zip(attributes).to_h
-          end
-
-          def event_id
-            self.class.event_id
-          end
-        end
+      def event_id
+        self.class.event_id
       end
 
       def inspect
