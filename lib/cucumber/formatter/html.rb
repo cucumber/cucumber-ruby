0badcff7c142d1c569676da23c600c72f233764e
Luke Hill <lukehill_uk@hotmail.com>
2026-06-04 16:20:20 +0100

    Tidy up formatters that dont need query / repository

--- lib/cucumber/formatter/html.rb
+++ lib/cucumber/formatter/html.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'cucumber/query'
 require 'cucumber/html_formatter'
 
 require_relative 'io'
@@ -12,8 +11,6 @@ module Cucumber
 
       def initialize(config)
         @io = ensure_io(config.out_stream, config.error_stream)
-        @repository = Cucumber::Repository.new
-        @query = Cucumber::Query.new(@repository)
         @html_formatter = Cucumber::HTMLFormatter::Formatter.new(@io)
         @html_formatter.write_pre_message
         config.on_event :envelope, &method(:output_envelope)
@@ -21,8 +18,8 @@ module Cucumber
 
       def output_envelope(event)
         envelope = event.envelope
-        @repository.update(envelope)
         @html_formatter.write_message(envelope)
+        # TODO: Move this conditional logic into the HTML formatter proper
         @html_formatter.write_post_message if envelope.test_run_finished
       end
     end
